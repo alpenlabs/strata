5ec8a08181a06c554e690def44ff9af79f076629
Trey Del Bonis <trey@alpenlabs.io>
2024-06-05 17:36:00 -0400

    sequencer: rework to use anyhow start opening databases

--- sequencer/src/args.rs
+++ sequencer/src/args.rs
@@ -1,8 +1,17 @@
+use std::path::PathBuf;
+
 use argh::FromArgs;
 
 #[derive(FromArgs)]
 #[argh(description = "Alpen Vertex sequencer")]
 pub struct Args {
+    #[argh(
+        option,
+        short = 'd',
+        description = "datadir path that will contain databases"
+    )]
+    pub datadir: PathBuf,
+
     #[argh(option, short = 'B', description = "bitcoind connection url")]
     pub bitcoind: String,
 
--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,5 +1,9 @@
+use std::io;
 use std::process;
+use std::sync::Arc;
 
+use anyhow::Context;
+use reth_rpc_types::ParseBlockHashOrNumberError;
 use thiserror::Error;
 use tokio::sync::oneshot;
 use tracing::*;
@@ -14,14 +18,30 @@ mod rpc_server;
 
 #[derive(Debug, Error)]
 pub enum InitError {
+    #[error("io: {0}")]
+    Io(#[from] io::Error),
+
     #[error("{0}")]
     Other(String),
 }
 
 fn main() {
+    let args: Args = argh::from_env();
+    if let Err(e) = main_inner(args) {
+        eprintln!("FATAL ERROR: {e}");
+    }
+}
+
+fn main_inner(args: Args) -> anyhow::Result<()> {
     logging::init();
 
-    let args = argh::from_env();
+    // Open the database.
+    let rbdb = open_rocksdb_database(&args)?;
+
+    // Initialize stubs.
+    let sync_ev_db = alpen_vertex_db::SyncEventDb::new(rbdb.clone());
+    let cs_db = alpen_vertex_db::ConsensusStateDb::new(rbdb.clone());
+    let l1_db = alpen_vertex_db::L1Db::new(rbdb.clone());
 
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
@@ -31,13 +51,14 @@ fn main() {
 
     if let Err(e) = rt.block_on(main_task(args)) {
         error!(err = %e, "main task exited");
-        process::exit(0);
+        process::exit(0); // special case exit once we've gotten to this point
     }
 
     info!("exiting");
+    Ok(())
 }
 
-async fn main_task(args: Args) -> Result<(), InitError> {
+async fn main_task(args: Args) -> anyhow::Result<()> {
     let (stop_tx, stop_rx) = oneshot::channel();
 
     // Init RPC methods.
@@ -62,4 +83,23 @@ async fn main_task(args: Args) -> Result<(), InitError> {
     }
 
     Ok(())
+}
+
+fn open_rocksdb_database(args: &Args) -> anyhow::Result<Arc<rockbound::DB>> {
+    let mut database_dir = args.datadir.clone();
+    database_dir.push("rocksdb");
+
+    let dbname = alpen_vertex_db::ROCKSDB_NAME;
+    let cfs = alpen_vertex_db::STORE_COLUMN_FAMILIES;
+    let opts = rocksdb::Options::default();
+
+    let rbdb = rockbound::DB::open(
+        &database_dir,
+        dbname,
+        cfs.iter().map(|s| s.to_string()),
+        &opts,
+    )
+    .context("opening database")?;
+
+    Ok(Arc::new(rbdb))
 }
