4d7ce44f02930cc24d4d5c1de969659d26724d99
Sapin Bajracharya <sapin@alpenlabs.io>
2024-06-10 14:51:21 +0545

    wrap http_client inside mockable trait add basic unit tests

--- crates/evmexec/src/engine.rs
+++ crates/evmexec/src/engine.rs
@@ -1,3 +1,6 @@
+use std::future::Future;
+use std::pin::Pin;
+
 use crate::auth_client_layer::{AuthClientLayer, AuthClientService};
 use crate::el_payload::ElPayload;
 use crate::get_runtime;
@@ -6,16 +9,93 @@ use alpen_vertex_evmctl::engine::{BlockStatus, ExecEngineCtl, PayloadStatus};
 use alpen_vertex_evmctl::errors::{EngineError, EngineResult};
 use alpen_vertex_evmctl::messages::{ELDepositData, ExecPayloadData, Op, PayloadEnv};
 use alpen_vertex_state::block::L2BlockId;
-use jsonrpsee::http_client::{transport::HttpBackend, HttpClient, HttpClientBuilder};
+use jsonrpsee::http_client::HttpClient;
+use jsonrpsee::http_client::{transport::HttpBackend, HttpClientBuilder};
 use reth_node_ethereum::EthEngineTypes;
 use reth_primitives::{Address, B256};
 use reth_rpc::JwtSecret;
 use reth_rpc_api::EngineApiClient;
 use reth_rpc_types::engine::{
-    ExecutionPayloadEnvelopeV2, ExecutionPayloadFieldV2, ExecutionPayloadInputV2, ForkchoiceState, PayloadAttributes, PayloadId, PayloadStatusEnum
+    ExecutionPayloadEnvelopeV2, ExecutionPayloadFieldV2, ExecutionPayloadInputV2, ForkchoiceState,
+    ForkchoiceUpdated, PayloadAttributes, PayloadId, PayloadStatusEnum,
 };
 use reth_rpc_types::Withdrawal;
 use tokio::sync::Mutex;
+
+pub trait HttpClientWrap {
+    fn fork_choice_updated_v2<'a>(
+        &'a self,
+        fork_choice_state: ForkchoiceState,
+        payload_attributes: Option<PayloadAttributes>,
+    ) -> Pin<Box<dyn Future<Output = Result<ForkchoiceUpdated, jsonrpsee::core::ClientError>> + 'a>>;
+
+    fn get_payload_v2<'a>(
+        &'a self,
+        payload_id: PayloadId,
+    ) -> Pin<
+        Box<
+            dyn Future<Output = Result<ExecutionPayloadEnvelopeV2, jsonrpsee::core::ClientError>>
+                + 'a,
+        >,
+    >;
+
+    fn new_payload_v2<'a>(
+        &'a self,
+        payload: ExecutionPayloadInputV2,
+    ) -> Pin<
+        Box<
+            dyn Future<
+                    Output = Result<
+                        reth_rpc_types::engine::PayloadStatus,
+                        jsonrpsee::core::ClientError,
+                    >,
+                > + 'a,
+        >,
+    >;
+}
+
+pub struct HttpClientWrapper {
+    pub(crate) client: HttpClient<AuthClientService<HttpBackend>>,
+}
+
+impl HttpClientWrap for HttpClientWrapper {
+    fn fork_choice_updated_v2<'a>(
+        &'a self,
+        fork_choice_state: ForkchoiceState,
+        payload_attributes: Option<PayloadAttributes>,
+    ) -> Pin<Box<dyn Future<Output = Result<ForkchoiceUpdated, jsonrpsee::core::ClientError>> + 'a>>
+    {
+        <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<EthEngineTypes>>::fork_choice_updated_v2::<'_, '_>(&self.client, fork_choice_state, payload_attributes)
+    }
+
+    fn get_payload_v2<'a>(
+        &'a self,
+        payload_id: PayloadId,
+    ) -> Pin<
+        Box<
+            dyn Future<Output = Result<ExecutionPayloadEnvelopeV2, jsonrpsee::core::ClientError>>
+                + 'a,
+        >,
+    > {
+        <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<EthEngineTypes>>::get_payload_v2::<'_, '_>(&self.client, payload_id)
+    }
+
+    fn new_payload_v2<'a>(
+        &'a self,
+        payload: ExecutionPayloadInputV2,
+    ) -> Pin<
+        Box<
+            dyn Future<
+                    Output = Result<
+                        reth_rpc_types::engine::PayloadStatus,
+                        jsonrpsee::core::ClientError,
+                    >,
+                > + 'a,
+        >,
+    > {
+        <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<EthEngineTypes>>::new_payload_v2::<'_, '_>(&self.client, payload)
+    }
+}
 
 fn http_client(http_url: &str, secret_hex: &str) -> HttpClient<AuthClientService<HttpBackend>> {
     let secret = JwtSecret::from_hex(secret_hex).unwrap();
@@ -42,23 +122,28 @@ pub struct ForkchoiceStatePartial {
     pub finalized_block_hash: Option<B256>,
 }
 
-pub struct RpcExecEngineCtl {
-    client: HttpClient<AuthClientService<HttpBackend>>,
+pub struct RpcExecEngineCtl<T> {
+    client: T,
     fork_choice_state: Mutex<ForkchoiceState>,
 }
 
-impl RpcExecEngineCtl {
-    pub fn new(http_url: &str, secret: &str, fork_choice_state: ForkchoiceState) -> Self {
-        Self {
+impl<T> RpcExecEngineCtl<T> {
+    pub fn from_url_secret(
+        http_url: &str,
+        secret: &str,
+        fork_choice_state: ForkchoiceState,
+    ) -> RpcExecEngineCtl<HttpClientWrapper> {
+        let client_wrapped = HttpClientWrapper {
             client: http_client(http_url, secret),
+        };
+        RpcExecEngineCtl::<HttpClientWrapper> {
+            client: client_wrapped,
             fork_choice_state: Mutex::new(fork_choice_state),
         }
     }
+}
 
-    fn engine_api_client(&self) -> &impl EngineApiClient<EthEngineTypes> {
-        &self.client
-    }
-
+impl<T: HttpClientWrap> RpcExecEngineCtl<T> {
     async fn update_block_state(
         &self,
         fcs_partial: ForkchoiceStatePartial,
@@ -79,11 +164,12 @@ impl RpcExecEngineCtl {
         };
 
         let fork_choice_result = self
-            .engine_api_client()
+            .client
             .fork_choice_updated_v2(fork_choice_state, None)
             .await;
 
-        let update_status = fork_choice_result.map_err(|err| EngineError::Other(err.to_string()))?;
+        let update_status =
+            fork_choice_result.map_err(|err| EngineError::Other(err.to_string()))?;
 
         match update_status.payload_status.status {
             PayloadStatusEnum::Valid => {
@@ -123,7 +209,7 @@ impl RpcExecEngineCtl {
         };
 
         let forkchoice_result = self
-            .engine_api_client()
+            .client
             .fork_choice_updated_v2(
                 self.fork_choice_state.lock().await.clone(),
                 Some(payload_attributes),
@@ -142,10 +228,7 @@ impl RpcExecEngineCtl {
 
     async fn get_payload_status(&self, payload_id: u64) -> EngineResult<PayloadStatus> {
         let payload_id_bytes = PayloadId::new(payload_id.to_be_bytes());
-        let payload_result: Result<ExecutionPayloadEnvelopeV2, jsonrpsee::core::ClientError> = self
-            .engine_api_client()
-            .get_payload_v2(payload_id_bytes)
-            .await;
+        let payload_result = self.client.get_payload_v2(payload_id_bytes).await;
 
         let payload = payload_result.map_err(|_| EngineError::UnknownPayloadId(payload_id))?;
 
@@ -175,9 +258,11 @@ impl RpcExecEngineCtl {
     }
 
     async fn submit_new_payload(&self, payload: ExecPayloadData) -> EngineResult<BlockStatus> {
-        let el_payload = borsh::from_slice::<ElPayload>(&payload.el_payload).map_err(|_| EngineError::Other("Invalid payload".to_string()))?;
+        let el_payload = borsh::from_slice::<ElPayload>(&payload.el_payload)
+            .map_err(|_| EngineError::Other("Invalid payload".to_string()))?;
 
-        let withdrawals: Vec<Withdrawal> = payload.ops
+        let withdrawals: Vec<Withdrawal> = payload
+            .ops
             .iter()
             .filter_map(|op| match op {
                 Op::Deposit(deposit_data) => Some(Withdrawal {
@@ -190,12 +275,17 @@ impl RpcExecEngineCtl {
 
         let v2_payload = ExecutionPayloadInputV2 {
             execution_payload: el_payload.into(),
-            withdrawals: if withdrawals.is_empty() { None } else { Some(withdrawals) }
+            withdrawals: if withdrawals.is_empty() {
+                None
+            } else {
+                Some(withdrawals)
+            },
         };
-        
-        let payload_status_result = self.engine_api_client().new_payload_v2(v2_payload).await;
 
-        let payload_status = payload_status_result.map_err(|err| EngineError::Other(err.to_string()))?;
+        let payload_status_result = self.client.new_payload_v2(v2_payload).await;
+
+        let payload_status =
+            payload_status_result.map_err(|err| EngineError::Other(err.to_string()))?;
 
         match payload_status.status {
             PayloadStatusEnum::Valid => EngineResult::Ok(BlockStatus::Valid),
@@ -206,7 +296,7 @@ impl RpcExecEngineCtl {
     }
 }
 
-impl ExecEngineCtl for RpcExecEngineCtl {
+impl<T: HttpClientWrap> ExecEngineCtl for RpcExecEngineCtl<T> {
     fn submit_payload(&self, payload: ExecPayloadData) -> EngineResult<BlockStatus> {
         get_runtime().block_on(self.submit_new_payload(payload))
     }
@@ -250,3 +340,187 @@ impl ExecEngineCtl for RpcExecEngineCtl {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alpen_vertex_evmctl::errors::EngineResult;
+    use alpen_vertex_evmctl::messages::PayloadEnv;
+    use alpen_vertex_primitives::buf::Buf32;
+    use arbitrary::{Arbitrary, Unstructured};
+    use mockall::{mock, predicate::*};
+    use rand::{Rng, RngCore};
+    use reth_primitives::revm_primitives::FixedBytes;
+    use reth_primitives::{Bloom, Bytes, U256};
+    use reth_rpc_types::ExecutionPayloadV1;
+    use tokio::sync::Mutex;
+
+    mock! {
+        HttpClient {}
+        impl HttpClientWrap for HttpClient {
+            fn fork_choice_updated_v2<'a>(
+                &'a self,
+                fork_choice_state: ForkchoiceState,
+                payload_attributes: Option<PayloadAttributes>,
+            ) -> Pin<Box<dyn Future<Output = Result<ForkchoiceUpdated, jsonrpsee::core::ClientError>> + 'a>>;
+
+            fn get_payload_v2<'a>(
+                &'a self,
+                payload_id: PayloadId,
+            ) -> Pin<Box<dyn Future<Output = Result<ExecutionPayloadEnvelopeV2, jsonrpsee::core::ClientError>> + 'a>>;
+
+            fn new_payload_v2<'a>(
+                &'a self,
+                payload: ExecutionPayloadInputV2,
+            ) -> Pin<Box<dyn Future<Output = Result<reth_rpc_types::engine::PayloadStatus, jsonrpsee::core::ClientError>> + 'a>>;
+        }
+    }
+
+    fn random_el_payload() -> ElPayload {
+        let mut rand_data = vec![0u8; 1024];
+        rand::thread_rng().fill_bytes(&mut rand_data);
+        let mut unstructured = Unstructured::new(&rand_data);
+        ElPayload::arbitrary(&mut unstructured).unwrap()
+    }
+
+    fn random_execution_payload_v1() -> ExecutionPayloadV1 {
+        let mut rng = rand::thread_rng();
+
+        ExecutionPayloadV1 {
+            parent_hash: B256::random(),
+            fee_recipient: Address::random(),
+            state_root: B256::random(),
+            receipts_root: B256::random(),
+            logs_bloom: Bloom::random(),
+            prev_randao: B256::random(),
+            block_number: rng.gen(),
+            gas_limit: 200000u64,
+            gas_used: 10000u64,
+            timestamp: rng.gen(),
+            extra_data: Bytes::new(),
+            base_fee_per_gas: U256::from(50),
+            block_hash: B256::random(),
+            transactions: vec![],
+        }
+    }
+
+    #[tokio::test]
+    async fn test_update_block_state() {
+        let mut mock_client = MockHttpClient::new();
+
+        let fcs_partial = ForkchoiceStatePartial {
+            head_block_hash: Some(B256::random()),
+            safe_block_hash: None,
+            finalized_block_hash: None,
+        };
+
+        let fcs = ForkchoiceState {
+            head_block_hash: B256::random(),
+            safe_block_hash: B256::random(),
+            finalized_block_hash: B256::random(),
+        };
+
+        mock_client
+            .expect_fork_choice_updated_v2()
+            .returning(move |_, _| {
+                Box::pin(async { Ok(ForkchoiceUpdated::from_status(PayloadStatusEnum::Valid)) })
+            });
+
+        let rpc_exec_engine_ctl = RpcExecEngineCtl {
+            client: mock_client,
+            fork_choice_state: Mutex::new(fcs),
+        };
+
+        let result = rpc_exec_engine_ctl.update_block_state(fcs_partial).await;
+
+        assert!(matches!(result, EngineResult::Ok(BlockStatus::Valid)));
+    }
+
+    #[tokio::test]
+    async fn test_build_block_from_mempool() {
+        let mut mock_client = MockHttpClient::new();
+        let fcs = ForkchoiceState::default();
+
+        mock_client
+            .expect_fork_choice_updated_v2()
+            .returning(move |_, _| {
+                Box::pin(async {
+                    Ok(ForkchoiceUpdated::from_status(PayloadStatusEnum::Valid)
+                        .with_payload_id(PayloadId::new([1u8; 8])))
+                })
+            });
+
+        let rpc_exec_engine_ctl = RpcExecEngineCtl {
+            client: mock_client,
+            fork_choice_state: Mutex::new(fcs),
+        };
+
+        let payload_env = PayloadEnv {
+            timestamp: 0,
+            el_ops: vec![],
+            safe_l1_block: Buf32(FixedBytes::<32>::random()),
+            prev_global_state_root: Buf32(FixedBytes::<32>::random()),
+        };
+
+        let result = rpc_exec_engine_ctl
+            .build_block_from_mempool(payload_env)
+            .await;
+
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_get_payload_status() {
+        let mut mock_client = MockHttpClient::new();
+        let fcs = ForkchoiceState::default();
+
+        mock_client.expect_get_payload_v2().returning(move |_| {
+            Box::pin(async {
+                Ok(ExecutionPayloadEnvelopeV2 {
+                    execution_payload: ExecutionPayloadFieldV2::V1(random_execution_payload_v1()),
+                    block_value: U256::from(100),
+                })
+            })
+        });
+
+        let rpc_exec_engine_ctl = RpcExecEngineCtl {
+            client: mock_client,
+            fork_choice_state: Mutex::new(fcs),
+        };
+
+        let result = rpc_exec_engine_ctl.get_payload_status(0).await;
+
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_submit_new_payload() {
+        let mut mock_client = MockHttpClient::new();
+        let fcs = ForkchoiceState::default();
+
+        let el_payload = random_el_payload();
+        let ops = vec![Op::Deposit(ELDepositData {
+            amt: 10000,
+            dest_addr: [1u8; 20].to_vec(),
+        })];
+
+        let payload_data = ExecPayloadData::new(borsh::to_vec(&el_payload).unwrap(), ops);
+
+        mock_client.expect_new_payload_v2().returning(move |_| {
+            Box::pin(async {
+                Ok(reth_rpc_types::engine::PayloadStatus {
+                    status: PayloadStatusEnum::Valid,
+                    latest_valid_hash: None,
+                })
+            })
+        });
+
+        let rpc_exec_engine_ctl = RpcExecEngineCtl {
+            client: mock_client,
+            fork_choice_state: Mutex::new(fcs),
+        };
+
+        let result = rpc_exec_engine_ctl.submit_new_payload(payload_data).await;
+
+        assert!(matches!(result, EngineResult::Ok(BlockStatus::Valid)));
+    }
+}
