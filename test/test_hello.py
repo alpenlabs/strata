45c5f2c7fd552a2bb18587cb8182f541ca91a222
Trey Del Bonis <trey@alpenlabs.io>
2024-06-21 18:29:24 -0400

    rpc/api, test: fixed names in RPC method attribs, added more verbose "hello" test output

--- test/test_hello.py
+++ test/test_hello.py
@@ -11,4 +11,8 @@ class HelloTest(flexitest.Test):
 
         seqrpc = seq.create_rpc()
         proto_ver = seqrpc.alp_protocolVersion()
-        print(proto_ver)
+        print("protocol version", proto_ver)
+        assert proto_ver == 1, "query protocol version"
+
+        l1stat = seqrpc.alp_l1status()
+        print("L1 status", l1stat)
