0d00031b3919b46b06369e811e46edb9caa561fe
voidash <ashish.thapa477@gmail.com>
2024-07-11 18:33:10 +0545

    db: implement ChainstateStore and Chainstateprovider

--- crates/db/src/chain_state/db.rs
+++ crates/db/src/chain_state/db.rs
@@ -41,9 +41,7 @@ impl ChainStateDB {
 
 impl ChainstateStore for ChainStateDB {
     fn write_genesis_state(&self, toplevel: &ChainState) -> DbResult<()> {
-        let mut batch = SchemaBatch::new(); 
-        batch.put::<ChainState>(&0, &toplevel.clone())?;
-        self.db.write_schemas(batch)?;
+        self.db.put::<ChainState>(&0, &toplevel.clone())?;
         Ok(())
     }
 
@@ -76,25 +74,83 @@ impl ChainstateStore for ChainStateDB {
         }
 
         // let iterator = self.db;
-        let mut iterator = self.db.iter::<ChainStateSchema>()?;
-        iterator.seek(&before_idx);
+        let mut cs_iterator = self.db.iter::<ChainStateSchema>()?;
+        cs_iterator.seek(&before_idx);
 
         let mut batch = SchemaBatch::new();
-        while let Some(cs_iter) = iterator.next() {
-            let (cur_state_key,_) = *cs_iter?;
-
-            batch.delete::<ChainStateSchema>(&cur_chain_state.key)?;
+        while let Some(cs) = cs_iterator.next() {
+            let (cs_key,_) = *cs?;
+            if cs_key < before_idx {
+                batch.delete::<ChainStateSchema>(cs_key)?;
+                batch.delete::<ChainWriteBatchSchema>(cs_key)?;
+            }
         }
-        
 
+        // let mut cs_batch_iterator = self.db.iter::<ChainWriteBatchSchema>()?;
+        // while let Some(cs) = cs_batch_iterator.next() {
+        //     let (cs_key,_) = *cs?;
+        //     if cs_key < before_idx {
+        //         batch.delete::<ChainWriteBatchSchema>(cs_key)?;
+        //     }
+        // }
 
+        //TODO: finalize batch
         
-        todo!()
+        Ok(())
     }
 
     fn rollback_writes_to(&self, new_tip_idx: u64) -> DbResult<()> {
-        todo!()
+        // the key should already be in the DB
+        let top_level = self.db.get::<ChainStateSchema>(&before_idx)?;
+        if let None = top_level {
+            return Err(DbError::UnknownIdx(before_idx));
+        }
+
+        let last_idx = self.find_last_write_batch();
+        if new_tip_idx > last_idx {
+            return Err(DbError::RevertAboveCurrent);
+        }
+        
+        let mut batch = SchemaBatch::new();
+        let mut cs_iterator = self.db.iter::<ChainStateSchema>()?.rev();
+        while let Some(cs) = cs_batch_iterator.next() {
+            let (cs_key, _) = *cs?;
+            if cs_key <= new_tip_idx {
+                break;
+            }
+            batch.delete::<ChainStateSchema>(cs_key);
+            batch.delete::<ChainWriteBatchSchema>(cs_key);
+            // TODO: we have to check if the data is present in the DB or not before deleting 
+        }
+        // TODO: Apply batch and check we are where we should be 
+
+        Ok(())
     }
 }
 
+impl ChainstateProvider for ChainStateDB {
+    fn get_last_state_idx(&self) -> DbResult<u64> {
+        let last_idx = self.find_last_write_batch()?.expect("CS not written");
+        Ok(last_idx)
+    }
 
+    fn get_earliest_state_idx(&self) -> DbResult<u64> {
+        let cs_iterator = self.db.iter::<ChainStateSchema>()?;
+        if let Some(cs) = cs_iterator.next() {
+            let (key,_) = cs?;
+            Ok(key)
+        }
+        //TODO: identify the error
+        Err(DbError::Unimplemented)
+    }
+
+    fn get_writes_at(&self, idx: u64) -> DbResult<Option<WriteBatch>> {
+        Ok(self.db.get::<ChainWriteBatchSchema>(&idx)?)
+    }
+
+    fn get_toplevel_state(&self, idx: u64) -> DbResult<Option<ChainState>> {
+        Ok(self.db.get::<ChainState>(&idx)?)
+    }
+}
+
+// TODO: Test
