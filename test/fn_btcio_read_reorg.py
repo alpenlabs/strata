5670981bcdbf82e901bd5c971e515bf80c57055c
Trey Del Bonis <trey@alpenlabs.io>
2024-07-22 14:46:21 -0400

    test: fix alp_ RPC name in fn_btcio_read_reorg

---
+++ test/fn_btcio_read_reorg.py
@@ -0,0 +1,40 @@
+import time
+
+import flexitest
+from bitcoinlib.services.bitcoind import BitcoindClient
+
+from constants import BLOCK_GENERATION_INTERVAL_SECS, SEQ_SLACK_TIME_SECS
+from entry import BasicEnvConfig
+
+
+@flexitest.register
+class L1ReadReorgTest(flexitest.Test):
+    REORG_DEPTH = 3
+
+    def __init__(self, ctx: flexitest.InitContext):
+        # standalone env for this test as it involves mutating the blockchain via invalidation
+        ctx.set_env(BasicEnvConfig())
+
+    def main(self, ctx: flexitest.RunContext):
+        btc = ctx.get_service("bitcoin")
+        seq = ctx.get_service("sequencer")
+
+        btcrpc: BitcoindClient = btc.create_rpc()
+        seqrpc = seq.create_rpc()
+
+        # We need at least `REORG_DEPTH` + 1 or more blocks
+        # to invalidate `REORG_DEPTH` blocks at the end.
+        wait_time = BLOCK_GENERATION_INTERVAL_SECS * (self.REORG_DEPTH + 1) + SEQ_SLACK_TIME_SECS
+        time.sleep(wait_time)
+        l1stat = seqrpc.alp_l1status()
+
+        height_to_invalidate_from = int(l1stat["cur_height"]) - self.REORG_DEPTH
+        block_to_invalidate_from = btcrpc.proxy.getblockhash(height_to_invalidate_from)
+        to_be_invalid_block = seqrpc.alp_getL1blockHash(height_to_invalidate_from + 1)
+        btcrpc.proxy.invalidateblock(block_to_invalidate_from)
+
+        # Wait for at least 1 block to be added after invalidating `REORG_DEPTH` blocks.
+        time.sleep(BLOCK_GENERATION_INTERVAL_SECS * 1 + SEQ_SLACK_TIME_SECS)
+        block_from_invalidated_height = seqrpc.alp_getL1blockHash(height_to_invalidate_from + 1)
+
+        assert to_be_invalid_block != block_from_invalidated_height, "Expected reorg from 3rd Block"
