f7dd737aa94d86fd9367b94e90a4cca4c858c699
Rajil <rajil@alpenlabs.io>
2024-07-23 14:37:53 +0545

    tests: increase waiting time for client start

--- test/fn_client_status.py
+++ test/fn_client_status.py
@@ -17,7 +17,7 @@ class HelloTest(flexitest.Test):
         print("protocol version", proto_ver)
         assert proto_ver == 1, "query protocol version"
 
-        time.sleep(1)
+        time.sleep(2)
         client_status = seqrpc.alp_clientStatus()
         print("client status", client_status)
 
