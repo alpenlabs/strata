ecd8173d8b69a20c65bd1e8f435e4a3252230cfa
Sapin Bajracharya <sapin@alpenlabs.io>
2024-07-29 13:54:27 +0545

    refactor: rename fcs -> fork_choice_state

--- crates/evmexec/src/lib.rs
+++ crates/evmexec/src/lib.rs
@@ -1,10 +1,10 @@
 mod block;
 mod el_payload;
-mod fcs;
+mod fork_choice_state;
 mod http_client;
 
 pub mod engine;
 pub mod preloaded_storage;
 
-pub use fcs::fork_choice_state_initial;
+pub use fork_choice_state::fork_choice_state_initial;
 pub use http_client::EngineRpcClient;
