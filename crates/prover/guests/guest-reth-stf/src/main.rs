3caece7cfecaeb0f7f088bfe3b47366f46aeaba9
abishek <abishek@alpenlabs.io>
2024-08-01 02:58:29 +0545

    prover: prover manager test done

--- crates/prover/guests/guest-reth-stf/src/main.rs
+++ crates/prover/guests/guest-reth-stf/src/main.rs
@@ -4,8 +4,6 @@ use revm::primitives::SpecId;
 use zkvm_primitives::processor::EvmConfig;
 use zkvm_primitives::{process_block_transaction, ZKVMInput};
 
-const ENCODED_INPUT: &[u8] = include_bytes!("../1.bin");
-
 // TODO: Read the evm config from the genesis config. This should be done in compile time.
 const EVM_CONFIG: EvmConfig = EvmConfig {
     chain_id: 12345,
@@ -13,8 +11,7 @@ const EVM_CONFIG: EvmConfig = EvmConfig {
 };
 
 fn main() {
-    // TODO: Read the input from the host
-    let input: ZKVMInput = bincode::deserialize(ENCODED_INPUT).unwrap();
+    let input: ZKVMInput = env::read();
 
     // Handle the block validation
     let public_params = process_block_transaction(input, EVM_CONFIG);
