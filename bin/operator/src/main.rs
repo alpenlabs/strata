cfc2c8c74588fe26738d099d2e8444ada472bcd3
Rajil <rajil@alpenlabs.io>
2024-07-31 17:07:37 +0545

    chore(bridge): add main fn stubs

--- bin/operator/src/main.rs
+++ bin/operator/src/main.rs
@@ -3,5 +3,6 @@
 //! Responsible for facilitating bridge-in and bridge-out operations by creating, storing and
 //! publishing appropriate transactions. Can also perform challenger duties.
 
-mod cli;
-mod config;
+fn main() {
+    todo!("I'm the oeprator. Implement me!");
+}
