43f0f1f1ce0fc20ba41eeadd06ca0d07911593c6
voidash <ashish.thapa477@gmail.com>
2024-07-31 23:39:42 +0545

    db: add feature flag, add rocksdb error type for DbError

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -2,8 +2,10 @@
 
 pub mod database;
 pub mod errors;
-pub mod stubs;
 pub mod traits;
 pub mod types;
+
+#[cfg(feature = "stubs")]
+pub mod stubs;
 
 pub type DbResult<T> = anyhow::Result<T, errors::DbError>;
