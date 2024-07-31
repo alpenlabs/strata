24dbe055315b73199139007aff3b0a2fcd4c7603
abishek <abishek@alpenlabs.io>
2024-08-01 01:22:10 +0545

    prover evm guest added

--- crates/prover/guests/guest-reth-stf/src/main.rs
+++ crates/prover/guests/guest-reth-stf/src/main.rs
@@ -1,32 +1,23 @@
 use risc0_zkvm::guest::env;
 
-use revm::InMemoryDB;
-use zkvm_primitives::db::InMemoryDBHelper;
-use zkvm_primitives::processor::EvmProcessor;
-use zkvm_primitives::ZKVMInput;
+use revm::primitives::SpecId;
+use zkvm_primitives::processor::EvmConfig;
+use zkvm_primitives::{process_block_transaction, ZKVMInput};
 
-const ENCODED_IP: &[u8] = include_bytes!("../1.bin");
+const ENCODED_INPUT: &[u8] = include_bytes!("../1.bin");
+
+// TODO: Read the evm config from the genesis config. This should be done in compile time.
+const EVM_CONFIG: EvmConfig = EvmConfig {
+    chain_id: 12345,
+    spec_id: SpecId::SHANGHAI,
+};
 
 fn main() {
     // TODO: Read the input from the host
-    let mut input: ZKVMInput = bincode::deserialize(ENCODED_IP).unwrap();
+    let input: ZKVMInput = bincode::deserialize(ENCODED_INPUT).unwrap();
 
-    // Initialize the database.
-    let db = InMemoryDB::initialize(&mut input).unwrap();
+    // Handle the block validation
+    let public_params = process_block_transaction(input, EVM_CONFIG);
 
-    // Execute the block.
-    let mut executor = EvmProcessor::<InMemoryDB> {
-        input,
-        db: Some(db),
-        header: None,
-    };
-
-    executor.initialize();
-    executor.execute();
-    executor.finalize();
-
-    // extract the public output
-    let res = executor.header.unwrap().state_root;
-
-    env::commit(&res);
+    env::commit(&public_params);
 }
