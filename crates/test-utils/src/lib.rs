8d6930611d2d85332a65cedd72c27d1b159a20b5
Trey Del Bonis <trey@alpenlabs.io>
2024-08-01 17:00:39 -0400

    test-utils: fix `ArbitraryGenerator` randomness

--- crates/test-utils/src/lib.rs
+++ crates/test-utils/src/lib.rs
@@ -1,3 +1,4 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 use alpen_express_db::database::CommonDatabase;
@@ -6,15 +7,18 @@ use alpen_express_db::stubs::{chain_state::StubChainstateDb, l2::StubL2Db};
 use alpen_express_rocksdb::{ClientStateDb, L1Db, SyncEventDb};
 
 use arbitrary::{Arbitrary, Unstructured};
-use rand::Rng;
+use rand::RngCore;
 use rockbound::rocksdb;
 use tempfile::TempDir;
 
 pub mod bitcoin;
 pub mod l2;
 
+const ARB_GEN_LEN: usize = 1 << 24; // 16 MiB
+
 pub struct ArbitraryGenerator {
-    buffer: Vec<u8>,
+    buf: Vec<u8>,
+    off: AtomicUsize,
 }
 
 impl Default for ArbitraryGenerator {
@@ -25,15 +29,26 @@ impl Default for ArbitraryGenerator {
 
 impl ArbitraryGenerator {
     pub fn new() -> Self {
+        Self::new_with_size(ARB_GEN_LEN)
+    }
+
+    pub fn new_with_size(n: usize) -> Self {
         let mut rng = rand::thread_rng();
-        // NOTE: 128 should be enough for testing purposes. Change to 256 as needed
-        let buffer: Vec<u8> = (0..128).map(|_| rng.gen()).collect();
-        ArbitraryGenerator { buffer }
+        let mut buf = vec![0; n];
+        rng.fill_bytes(&mut buf); // 128 wasn't enough
+        let off = AtomicUsize::new(0);
+        ArbitraryGenerator { buf, off }
     }
 
     pub fn generate<'a, T: Arbitrary<'a> + Clone>(&'a self) -> T {
-        let mut u = Unstructured::new(&self.buffer);
-        T::arbitrary(&mut u).expect("failed to generate arbitrary instance")
+        // Doing hacky atomics to make this actually be reusable, this is pretty bad.
+        let off = self.off.load(Ordering::Relaxed);
+        let mut u = Unstructured::new(&self.buf[off..]);
+        let prev_off = u.len();
+        let inst = T::arbitrary(&mut u).expect("failed to generate arbitrary instance");
+        let additional_off = prev_off - u.len();
+        self.off.store(off + additional_off, Ordering::Relaxed);
+        inst
     }
 }
 
