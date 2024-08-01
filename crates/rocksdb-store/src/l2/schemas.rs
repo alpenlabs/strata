345cc10b762ec44ef2c05b91035c56414074ba6f
Sapin Bajracharya <sapin@alpenlabs.io>
2024-08-01 18:41:46 +0545

    remove unused code

--- crates/rocksdb-store/src/l2/schemas.rs
+++ crates/rocksdb-store/src/l2/schemas.rs
@@ -1,17 +1,11 @@
+use alpen_express_db::traits::BlockStatus;
+use alpen_express_state::block::L2BlockBundle;
+use alpen_express_state::id::L2BlockId;
+
 use crate::define_table_with_default_codec;
 use crate::define_table_with_seek_key_codec;
 use crate::define_table_without_codec;
 use crate::impl_borsh_value_codec;
-use alpen_express_db::traits::BlockStatus;
-use alpen_express_state::block::L2BlockBundle;
-use alpen_express_state::id::L2BlockId;
-use borsh::BorshDeserialize;
-use borsh::BorshSerialize;
-
-#[derive(Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize)]
-pub struct SomeBlock {
-    name: String,
-}
 
 define_table_with_default_codec!(
     /// A table to store L2 Block data. Maps block id to Block
