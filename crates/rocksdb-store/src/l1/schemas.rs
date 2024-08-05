04257770d2e5e8f1f8a69d569c467f59cb46a9d4
Sapin Bajracharya <sapin@alpenlabs.io>
2024-08-05 14:49:54 +0545

    fix: use seekable key codec for storing L1 block data in rocksdb

--- crates/rocksdb-store/src/l1/schemas.rs
+++ crates/rocksdb-store/src/l1/schemas.rs
@@ -5,6 +5,7 @@ use alpen_express_primitives::{
 };
 
 use crate::define_table_with_default_codec;
+use crate::define_table_with_seek_key_codec;
 use crate::define_table_without_codec;
 use crate::impl_borsh_value_codec;
 
@@ -12,7 +13,7 @@ use crate::impl_borsh_value_codec;
 type HeaderHash = Buf32;
 
 // L1 Block Schema and corresponding codecs implementation
-define_table_with_default_codec!(
+define_table_with_seek_key_codec!(
     /// A table to store L1 Block data. Maps block index to header
     (L1BlockSchema) u64 => L1BlockManifest
 );
@@ -24,7 +25,7 @@ define_table_with_default_codec!(
 );
 
 // Mmr Schema and corresponding codecs implementation
-define_table_with_default_codec!(
+define_table_with_seek_key_codec!(
     /// A table to store L1 Headers mmr
     (MmrSchema) u64 => CompactMmr
 );
