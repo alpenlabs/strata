6386cc1bc14bae86ae279206f9f26bd05f206046
Sapin Bajracharya <sapin@alpenlabs.io>
2024-08-07 12:59:24 +0545

    remove commented code

--- crates/reth/db/src/rocksdb/schema.rs
+++ crates/reth/db/src/rocksdb/schema.rs
@@ -34,29 +34,6 @@ impl rockbound::schema::KeyDecoder<BlockWitnessSchema> for B256 {
             .map_err(Into::into)
     }
 }
-// impl rockbound::schema::ValueCodec<BlockWitnessSchema> for ZKVMInput {
-//     fn encode_value(&self) -> ::std::result::Result<::std::vec::Vec<u8>, rockbound::CodecError> {
-//         let bincode_options = ::bincode::options()
-//             .with_fixint_encoding()
-//             .with_big_endian();
-
-//         bincode_options
-//             .serialize(self)
-//             .context("Failed to serialize value")
-//             .map_err(Into::into)
-//     }
-
-//     fn decode_value(data: &[u8]) -> ::std::result::Result<Self, rockbound::CodecError> {
-//         let bincode_options = ::bincode::options()
-//             .with_fixint_encoding()
-//             .with_big_endian();
-
-//         bincode_options
-//             .deserialize_from(&mut &data[..])
-//             .context("Failed to deserialize value")
-//             .map_err(Into::into)
-//     }
-// }
 
 impl rockbound::schema::ValueCodec<BlockWitnessSchema> for Vec<u8> {
     fn encode_value(&self) -> ::std::result::Result<::std::vec::Vec<u8>, rockbound::CodecError> {
