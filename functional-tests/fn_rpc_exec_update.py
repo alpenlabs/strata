bb8f310e34292cf6809b1d32b1d5c8eb0afdba34
voidash <ashish.thapa477@gmail.com>
2024-08-07 19:26:05 +0545

    rpc: format files and minor changes

--- functional-tests/fn_rpc_exec_update.py
+++ functional-tests/fn_rpc_exec_update.py
@@ -4,10 +4,6 @@ from bitcoinlib.services.bitcoind import BitcoindClient
 import flexitest
 
 
-
-NO_OF_BLOCKS_TO_RECEIVE = 3
-BLOCK_NUMBER = 1
-
 @flexitest.register
 class ExecUpdateTest(flexitest.Test):
     def __init__(self, ctx: flexitest.InitContext):
@@ -19,17 +15,8 @@ class ExecUpdateTest(flexitest.Test):
         # create both btc and sequencer RPC
         seqrpc = seq.create_rpc()
         time.sleep(2)
-        recent_blks = seqrpc.alp_getRecentBlocks(NO_OF_BLOCKS_TO_RECEIVE)
 
-        assert len(recent_blks) == NO_OF_BLOCKS_TO_RECEIVE
-
-        print(recent_blks[BLOCK_NUMBER]["block_id"])
-        exec_update  = seqrpc.alp_getExecUpdateById(recent_blks[1]["block_id"])
-        assert exec_update["update_idx"] == BLOCK_NUMBER
-
-
-
-
-
-
-
+        recent_blks = seqrpc.alp_getRecentBlocks(1)
+        exec_update = seqrpc.alp_getExecUpdateById(recent_blks[0]["block_id"])
+        print(exec_update)
+        assert exec_update["update_idx"] == recent_blks[0]["block_idx"]
--- functional-tests/fn_rpc_recent_blocks.py
+++ functional-tests/fn_rpc_recent_blocks.py
@@ -2,11 +2,10 @@ import time
 
 from bitcoinlib.services.bitcoind import BitcoindClient
 import flexitest
-from seqrpc import RpcError
 
-
-NO_OF_BLOCKS_TO_RECEIVE = 10
+NUM_BLOCKS_TO_RECEIVE = 10
 BLOCK_NUMBER = 4
+
 
 @flexitest.register
 class RecentBlocksTest(flexitest.Test):
@@ -21,25 +20,25 @@ class RecentBlocksTest(flexitest.Test):
         counter = 0
         while True:
             try:
-                _ = seqrpc.alp_getBlocksAtIdx(NO_OF_BLOCKS_TO_RECEIVE)
-            except RpcError:
-                # cancel retrying if we encounter this for more than 20 times
+                _ = seqrpc.alp_getBlocksAtIdx(NUM_BLOCKS_TO_RECEIVE)
+            except:
+                # cancel retrying if we encounter this for more than 20 19:20s
                 if counter == 20:
                     break
                 time.sleep(1)
                 counter += 1
+                continue
+
             break
 
+        recent_blks = seqrpc.alp_getRecentBlocks(NUM_BLOCKS_TO_RECEIVE)
 
-        recent_blks = seqrpc.alp_getRecentBlocks(NO_OF_BLOCKS_TO_RECEIVE)
-
-        assert len(recent_blks) == NO_OF_BLOCKS_TO_RECEIVE
+        assert len(recent_blks) == NUM_BLOCKS_TO_RECEIVE
 
         # check if they are in order by verifying if N-1 block is parent of N block
-        for idx in range(0,NO_OF_BLOCKS_TO_RECEIVE):
-            if idx != NO_OF_BLOCKS_TO_RECEIVE-1:
-                assert recent_blks[idx]["prev_block"] == recent_blks[idx+1]["block_id"]
-
+        for idx in range(0, NUM_BLOCKS_TO_RECEIVE):
+            if idx != NUM_BLOCKS_TO_RECEIVE - 1:
+                assert recent_blks[idx]["prev_block"] == recent_blks[idx + 1]["block_id"]
 
         l2_blk = seqrpc.alp_getBlocksAtIdx(recent_blks[BLOCK_NUMBER]["block_idx"])
 
@@ -49,8 +48,3 @@ class RecentBlocksTest(flexitest.Test):
 
         # check if we got the correct block when looked using hash
         assert second_blk_from_id["block_id"] == l2_blk[0]["block_id"]
-
-
-
-
-
