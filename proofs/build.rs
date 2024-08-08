789d68c0f3669c7116f9b6559323ecb94441d69b
abishek <abishek@alpenlabs.io>
2024-08-08 09:56:48 +0545

    prover feature flag

--- proofs/build.rs
+++ proofs/build.rs
@@ -1,3 +1,19 @@
 fn main() {
-    risc0_build::embed_methods();
+    if cfg!(feature = "risc_zero") {
+        risc0_build::embed_methods();
+    } else if cfg!(feature = "sp1") {
+        todo!()
+    } else {
+        // Return mock ELF
+        let out_dir = std::env::var_os("OUT_DIR").unwrap();
+        let out_dir = std::path::Path::new(&out_dir);
+        let methods_path = out_dir.join("methods.rs");
+
+        let elf = r#"
+            pub const GUEST_RETH_STF_ELF: &[u8] = &[];
+            pub const GUEST_RETH_STF_ID: [u32; 8] = [0u32;8];
+        "#;
+
+        std::fs::write(methods_path, elf).expect("Failed to write mock rollup elf");
+    }
 }
