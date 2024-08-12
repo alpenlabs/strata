dda52d281d5e656f3cf837059a9d329bf97a1232
Prajwol <prajwol@alpenlabs.io>
2024-08-12 13:22:49 +0545

    functional-tests: fix lints

--- functional-tests/fn_el_block_witness_data.py
+++ functional-tests/fn_el_block_witness_data.py
@@ -1,9 +1,7 @@
+import random
 import time
 
 import flexitest
-import random
-
-from constants import MAX_HORIZON_POLL_INTERVAL_SECS, SEQ_SLACK_TIME_SECS
 
 
 @flexitest.register
@@ -20,21 +18,18 @@ class ElBlockWitnessDataGenerationTest(flexitest.Test):
 
         time.sleep(5)
 
-        last_blocknum = int(rethrpc.eth_blockNumber(), 16);
+        last_blocknum = int(rethrpc.eth_blockNumber(), 16)
 
         assert last_blocknum > 5, "dont have enough blocks generated"
 
-        blocknums = random.sample(range(1, last_blocknum + 1), 5);
+        blocknums = random.sample(range(1, last_blocknum + 1), 5)
 
         for blocknum in blocknums:
             blockhash = rethrpc.eth_getBlockByNumber(hex(blocknum), False)["hash"]
             witness_data = rethrpc.alpee_getBlockWitness(blockhash, False)
 
-            assert witness_data != None, "non empty witness"
+            assert witness_data is not None, "non empty witness"
 
             # TODO: check witness data is ok ?
 
             print(blocknum, witness_data)
-
-
-
--- functional-tests/fn_el_genesis.py
+++ functional-tests/fn_el_genesis.py
@@ -1,8 +1,4 @@
-import time
-
 import flexitest
-
-from constants import MAX_HORIZON_POLL_INTERVAL_SECS, SEQ_SLACK_TIME_SECS
 
 
 @flexitest.register
@@ -16,4 +12,5 @@ class ElGenesisTest(flexitest.Test):
         rethrpc = reth.create_rpc()
         genesis_block = rethrpc.eth_getBlockByNumber(hex(0), True)
 
-        assert genesis_block["hash"] == "0x37ad61cff1367467a98cf7c54c4ac99e989f1fbb1bc1e646235e90c065c565ba", "genesis block hash"
+        expected = "0x37ad61cff1367467a98cf7c54c4ac99e989f1fbb1bc1e646235e90c065c565ba"
+        assert genesis_block["hash"] == expected, "genesis block hash"
