8d0ba0353b9644bc72a062a0772230ff781b05e9
Rajil <rajil@alpenlabs.io>
2024-08-15 01:40:55 +0545

    fix(primitives): convert error type for borsh

--- bin/bridge-client/src/modes/operator/constants.rs
+++ bin/bridge-client/src/modes/operator/constants.rs
@@ -1,2 +1,2 @@
 pub(super) const RPC_PORT: usize = 4781; // first 4 digits in the sha256 of "operator"
-pub(super) const RPC_SERVER: &str = "127.0.0.1"; // first 4 digits in the sha256 of "operator"
+pub(super) const RPC_SERVER: &str = "127.0.0.1";
