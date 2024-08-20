b27dec91724ea727a4134e38f4fcf2bc7b888054
Rajil <rajil@alpenlabs.io>
2024-08-21 01:50:07 +0545

    refactor(primitives): rename `TweakedTrPubkey` to `XOnlyPk`

--- crates/state/src/bridge_state.rs
+++ crates/state/src/bridge_state.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 
 use alpen_express_primitives::{
     buf::Buf32,
-    l1::{self, BitcoinAmount, OutputRef, TweakedTrPubkey},
+    l1::{self, BitcoinAmount, OutputRef, XOnlyPk},
 };
 
 /// The bitcoin block height that a withdrawal command references.
@@ -243,8 +243,8 @@ pub struct DispatchCommand {
 /// An output constructed from [`crate::bridge_ops::WithdrawalIntent`].
 #[derive(Clone, Debug, Eq, PartialEq, BorshDeserialize, BorshSerialize)]
 pub struct WithdrawOutput {
-    /// Taproot pubkey with the merkle root information.
-    dest_addr: TweakedTrPubkey,
+    /// Taproot Schnorr XOnlyPubkey with the merkle root information.
+    dest_addr: XOnlyPk,
 
     /// Amount in sats.
     amt: BitcoinAmount,
