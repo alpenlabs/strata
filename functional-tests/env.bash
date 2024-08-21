03e7a018913b2d26863ee01b8641872c710bff6a
Jose Storopoli <jose@storopoli.io>
2024-08-21 14:40:13 -0300

    tests: remove unused comment

--- functional-tests/env.bash
+++ functional-tests/env.bash
@@ -1,5 +1,4 @@
-# export RUST_BACKTRACE=1
 export RUST_LOG=trace,hyper=warn,soketto=warn,jsonrpsee-server=warn,mio=warn
 export NO_COLOR=1
 export PATH=$PATH:$(realpath ../target/release)
-export RUST_BACKTRACE=1
+export RUST_BACKTRACE=full
