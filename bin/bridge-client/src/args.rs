70ff5e05c64abb271418076df63880f322b3634f
Rajil <rajil@alpenlabs.io>
2024-08-22 21:32:31 +0545

    refactor(bridge-client): convert `mode` to positional arg

--- bin/bridge-client/src/args.rs
+++ bin/bridge-client/src/args.rs
@@ -2,40 +2,40 @@
 use std::fmt::Display;
 use std::str::FromStr;
 
-use clap::builder;
-use clap::builder::TypedValueParser;
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Debug, Parser)]
 #[command(name = "express-bridge-client")]
 #[command(about = "The bridge client for Express")]
-pub(crate) struct Args {
+pub(crate) struct Cli {
     #[arg(
-        short = 'm',
-        long = "mode",
-        default_value_t = ModeOfOperation::Operator,
-        value_parser = builder::PossibleValuesParser::new(["operator", "challenger"])
-            .map(|s| ModeOfOperation::from_str(&s).unwrap())
+        value_enum,
+        help = "What mode to run the client in `Operator` (alias: op) or `Challenger` (alias: ch)"
     )]
-    pub mode: ModeOfOperation,
+    pub mode: OperationMode,
 }
 
-#[derive(Debug, Clone)]
-pub(super) enum ModeOfOperation {
+#[derive(Debug, Clone, ValueEnum, Parser)]
+pub(super) enum OperationMode {
+    /// Run client in Operator mode to handle deposits, withdrawals and challenging.
+    #[clap(alias = "op")]
     Operator,
+
+    /// Run client in Challenger mode to verify/challenge Operator claims.
+    #[clap(alias = "ch")]
     Challenger,
 }
 
-impl Display for ModeOfOperation {
+impl Display for OperationMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ModeOfOperation::Operator => write!(f, "operator"),
-            ModeOfOperation::Challenger => write!(f, "challenger"),
+            OperationMode::Operator => write!(f, "operator"),
+            OperationMode::Challenger => write!(f, "challenger"),
         }
     }
 }
 
-impl FromStr for ModeOfOperation {
+impl FromStr for OperationMode {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
