a992993a853815e0009650512df71ec6f934b881
abishek <abishek@alpenlabs.io>
2024-08-25 23:11:34 +0545

    prover sp1 guest builder

--- proofs/sp1/src/lib.rs
+++ proofs/sp1/src/lib.rs
@@ -1,6 +1,1 @@
-#[cfg(feature = "prover")]
-pub const GUEST_RETH_STF_ELF: &[u8] =
-    include_bytes!("../guest-reth-stf/elf/riscv32im-succinct-zkvm-elf");
-
-#[cfg(not(feature = "prover"))]
-pub const GUEST_RETH_STF_ELF: &[u8] = &[32; 8];
+include!(concat!(env!("OUT_DIR"), "/methods.rs"));
