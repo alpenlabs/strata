7620593aea28aecfbc4dbdbd7d6bb23b0cb3cd1c
abishek <abishek@alpenlabs.io>
2024-09-01 18:02:02 +0545

    prover cl block stf guest code init

--- proofs/risc0/build.rs
+++ proofs/risc0/build.rs
@@ -12,6 +12,7 @@ fn main() {
 
         let elf = r#"
             pub const RETH_RISC0_ELF: &[u8] = &[];
+            pub const CL_BLOCK_STF_ELF: &[u8] = &[];
         "#;
 
         std::fs::write(methods_path, elf).expect("Failed to write mock rollup elf");
