81dc1c06c1233b6a7252f3f0cd03dce17c34d9b7
Jose Storopoli <jose@storopoli.io>
2024-09-04 16:49:36 -0300

    feat(bridge-exec)!: get_operator_outpoint

--- crates/bridge-exec/src/withdrawal_handler/handler.rs
+++ crates/bridge-exec/src/withdrawal_handler/handler.rs
@@ -1,24 +1,42 @@
 //! Defines the functions that pertain to handling a withdrawal request.
 
-use bitcoin::{address::NetworkChecked, secp256k1::schnorr::Signature, Address, Network, OutPoint};
+use std::sync::Arc;
+
+use bitcoin::{
+    address::NetworkChecked, secp256k1::schnorr::Signature, Address, Network,
+    OutPoint,
+};
 
 use alpen_express_primitives::l1::BitcoinAmount;
 use express_bridge_txm::{ReimbursementRequest, SignatureInfo};
 
-use super::errors::WithdrawalExecResult;
+use crate::withdrawal_handler::errors::{WithdrawalExecError, WithdrawalExecResult};
 
 /// Get the outpoint used for front-payments during withdrawal from the supplied reserved
 /// address for the given network.
 ///
 /// This involves getting unspent UTXOs in the address and finding an outpoint with enough
 /// bitcoins to service the withdrawal via a transaction chain.
-// TODO: pass bitcoin rpc client after <https://github.com/alpenlabs/express/pull/239> is merged.
-pub fn get_operator_outpoint(
-    _reserved_address: Address<NetworkChecked>,
-    _network: Network,
-    _amount: BitcoinAmount,
-) -> OutPoint {
-    unimplemented!()
+pub async fn get_operator_outpoint(
+    reserved_address: Address<NetworkChecked>,
+    network: Network,
+    amount: BitcoinAmount,
+    rpc_client: Arc<impl rpc_client::BitcoinReader + rpc_client::BitcoinWallet>,
+) -> WithdrawalExecResult<OutPoint> {
+    let utxos = rpc_client
+        .get_utxos()
+        .await
+        .expect("Could not get UTXOs from the BitcoinWalletRPC") // FIXME: convert to `?` once #251 is merged
+        .into_iter()
+        .filter(|utxo| utxo.address == reserved_address);
+    let candidate_utxo = utxos.find(|utxo| utxo.amount.to_sat() >= amount.to_sat());
+    match candidate_utxo {
+        Some(utxo) => Ok(OutPoint {
+            txid: utxo.txid,
+            vout: utxo.vout,
+        }),
+        None => Err(WithdrawalExecError::InsufficientFunds),
+    }
 }
 
 /// Sign the reimbursement transaction.
