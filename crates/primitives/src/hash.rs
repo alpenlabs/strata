198dcdbb8842ff3d1245f22ae8de142d232c3ee0
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-09-05 17:39:40 +0545

    perf: use RustCrypto sha2 crate to improve prover performance

--- crates/primitives/src/hash.rs
+++ crates/primitives/src/hash.rs
@@ -1,6 +1,5 @@
 //! Common wrapper around whatever we choose our native hash function to be.
 
-use bitcoin::hashes::Hash;
 use borsh::BorshSerialize;
 use digest::Digest;
 use sha2::Sha256;
@@ -20,8 +19,20 @@ pub fn compute_borsh_hash<T: BorshSerialize>(v: &T) -> Buf32 {
     Buf32::from(arr)
 }
 
-/// Computes a Bitcoin-style double-SHA-256.
+/// Implements a double SHA256 (`Sha256d`) hashing function using [RustCrypto's SHA-2 crate](https://github.com/RustCrypto/hashes/tree/master/sha2).
+///
+/// This implementation is designed to be equivalent to the one found in the
+/// [`bitcoin_hashes` crate](https://github.com/rust-bitcoin/rust-bitcoin/blob/master/hashes/src/sha256d.rs)
+/// but is built upon the [RustCrypto's SHA-2 crate](https://github.com/RustCrypto/hashes/tree/master/sha2),
+/// because it has patches available from both the
+/// [Risc0](https://github.com/risc0/RustCrypto-hashes)
+/// and [Sp1](https://github.com/sp1-patches/RustCrypto-hashes)
+/// crates.
 pub fn sha256d(buf: &[u8]) -> Buf32 {
-    let h = bitcoin::hashes::sha256d::Hash::hash(buf);
-    h.to_byte_array().into()
+    let mut hasher = Sha256::new();
+    hasher.update(buf);
+    let result = hasher.finalize_reset();
+    hasher.update(result);
+    let arr: [u8; 32] = hasher.finalize().into();
+    Buf32::from(arr)
 }
