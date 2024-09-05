cae59beb91b3a0383d15cbe8bc963250eb163ecd
Rajil <rajil@alpenlabs.io>
2024-09-05 23:39:04 +0545

    chore(withdrawal-handler): update PR reference

--- crates/bridge-exec/src/withdrawal_handler/handler.rs
+++ crates/bridge-exec/src/withdrawal_handler/handler.rs
@@ -1,5 +1,7 @@
 //! Defines the functions that pertain to handling a withdrawal request.
 
+// TODO: there should be a database that tracks the status of tasks.
+
 use alpen_express_primitives::l1::BitcoinAmount;
 use bitcoin::{address::NetworkChecked, secp256k1::schnorr::Signature, Address, Network, OutPoint};
 use express_bridge_txm::{ReimbursementRequest, SignatureInfo};
@@ -11,7 +13,7 @@ use super::errors::WithdrawalExecResult;
 ///
 /// This involves getting unspent UTXOs in the address and finding an outpoint with enough
 /// bitcoins to service the withdrawal via a transaction chain.
-// TODO: pass bitcoin rpc client after <https://github.com/alpenlabs/express/pull/239> is merged.
+// TODO: pass bitcoin rpc client after <https://github.com/alpenlabs/express/pull/251> is merged.
 pub fn get_operator_outpoint(
     _reserved_address: Address<NetworkChecked>,
     _network: Network,
