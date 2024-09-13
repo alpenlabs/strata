4ee61fb2c33c513a506160c7ba10665d8d576dba
Bibek Pandey <bibek@alpenlabs.com>
2024-09-13 14:49:58 +0545

    functional-test: Remove redundant batch_commitment test

--- functional-tests/fn_sync_l1_finalization.py
+++ functional-tests/fn_sync_l1_finalization.py
@@ -5,8 +5,7 @@ import flexitest
 
 @flexitest.register
 class BlockFinalizationTest(flexitest.Test):
-    """
-    """
+    """ """
 
     def __init__(self, ctx: flexitest.InitContext):
         ctx.set_env("fast_batches")
@@ -23,9 +22,11 @@ class BlockFinalizationTest(flexitest.Test):
         checkpoint_info = seqrpc.alp_getCheckpointInfo(idx)
         print(f"checkpoint info for {idx}", checkpoint_info)
 
-        assert syncstat["finalized_block_id"] != checkpoint_info["l2_blockid"], "Checkpoint block should not yet finalize"
+        assert (
+            syncstat["finalized_block_id"] != checkpoint_info["l2_blockid"]
+        ), "Checkpoint block should not yet finalize"
 
-        checkpoint_info_1 = seqrpc.alp_getCheckpointInfo(idx+1)
+        checkpoint_info_1 = seqrpc.alp_getCheckpointInfo(idx + 1)
 
         assert checkpoint_info["idx"] == idx
         assert checkpoint_info_1 is None, "There should be no checkpoint info for idx 1"
@@ -33,7 +34,7 @@ class BlockFinalizationTest(flexitest.Test):
         to_finalize_blkid = checkpoint_info["l2_blockid"]
 
         # Post checkpoint proof
-        seqrpc.alp_putCheckpointProof(idx, [1,2,3,4]) ## TODO: hex
+        seqrpc.alp_putCheckpointProof(idx, [1, 2, 3, 4])  ## TODO: hex
 
         # Polling interval is 5 secs for sequencer so sleep 1 sec extra
         time.sleep(6)
