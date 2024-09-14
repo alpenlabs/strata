0d830748d43708210d2cd2ba9c0acad36f3153b1
Bibek Pandey <bibek@alpenlabs.com>
2024-09-14 09:59:24 +0545

    storage/managers: Add checkpoint manager

--- crates/storage/src/managers/mod.rs
+++ crates/storage/src/managers/mod.rs
@@ -1,1 +1,2 @@
+pub mod checkpoint;
 pub mod l2;
---
+++ crates/storage/src/ops/checkpoint.rs
@@ -0,0 +1,49 @@
+//! Checkpoint Proof data operation interface.
+
+use std::sync::Arc;
+
+use alpen_express_db::{
+    traits::*,
+    types::{CheckpointEntry, CheckpointStatus},
+};
+
+use crate::exec::*;
+
+/// Database context for an database operation interface.
+pub struct Context<D: Database> {
+    db: Arc<D>,
+}
+
+impl<D: Database + Sync + Send + 'static> Context<D> {
+    pub fn new(db: Arc<D>) -> Self {
+        Self { db }
+    }
+
+    pub fn into_ops(self, pool: threadpool::ThreadPool) -> CheckpointDataOps {
+        CheckpointDataOps::new(pool, Arc::new(self))
+    }
+}
+
+inst_ops! {
+    (CheckpointDataOps, Context<D: Database>) {
+        get_batch_checkpoint(idx: u64) => Option<CheckpointEntry>;
+        put_batch_checkpoint(idx: u64, entry: CheckpointEntry) => ();
+    }
+}
+
+fn get_batch_checkpoint<D: Database>(
+    context: &Context<D>,
+    idx: u64,
+) -> DbResult<Option<CheckpointEntry>> {
+    let checkpt_prov = context.db.checkpoint_provider();
+    checkpt_prov.get_batch_checkpoint(idx)
+}
+
+fn put_batch_checkpoint<D: Database>(
+    context: &Context<D>,
+    idx: u64,
+    entry: CheckpointEntry,
+) -> DbResult<()> {
+    let checkpt_store = context.db.checkpoint_store();
+    checkpt_store.put_batch_checkpoint(idx, entry)
+}
