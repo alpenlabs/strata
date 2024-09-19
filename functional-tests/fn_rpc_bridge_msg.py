6903806063d75f74e44bc4f51241dc3590b475c8
Trey Del Bonis <trey@alpenlabs.io>
2024-09-19 11:39:56 -0400

    fntest: added fn_rpc_bridge_msg

---
+++ functional-tests/fn_rpc_bridge_msg.py
@@ -0,0 +1,44 @@
+import time
+
+import flexitest
+
+WAIT_TIME = 2
+
+
+@flexitest.register
+class BridgeMsgTest(flexitest.Test):
+    def __init__(self, ctx: flexitest.InitContext):
+        ctx.set_env("basic")
+
+    def main(self, ctx: flexitest.RunContext):
+        seq = ctx.get_service("sequencer")
+
+        # create both btc and sequencer RPC
+        seqrpc = seq.create_rpc()
+
+        # BridgeMessage { source_id: 1,
+        #                 sig: [00] * 64
+        #                 scope: Misc, payload: [42] }
+        raw_msg = "".join(
+            [
+                "01000000",
+                "00" * 64,
+                "01000000" + "00",
+                "01000000" + "42",
+            ]
+        )
+
+        seqrpc.alp_submitBridgeMsg(raw_msg)
+
+        time.sleep(WAIT_TIME)
+
+        # VODepositSig(10)
+        scope = "00"
+        print(scope)
+
+        msgs = seqrpc.alp_getMsgsByScope(scope)
+        print(msgs)
+
+        # check if received blobdata and sent blobdata are same or not
+        assert len(msgs) == 1, "wrong number of messages in response"
+        assert msgs[0] == raw_msg, "not the message we expected"
