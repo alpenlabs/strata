32dc5635bc42a1e683da1749260a304e90f65939
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-09-19 20:55:57 +0545

    feat(checkpoint-proof): add sp1 code

--- provers/sp1/tests/checkpoint.rs
+++ provers/sp1/tests/checkpoint.rs
@@ -13,10 +13,9 @@ mod test {
         mock::get_verification_state_for_block,
         pow_params::PowParams,
     };
-    use express_risc0_adapter::{Risc0Verifier, RiscZeroHost, RiscZeroProofInputBuilder};
-    use express_risc0_guest_builder::{
-        GUEST_RISC0_BTC_BLOCKSPACE_ELF, GUEST_RISC0_BTC_BLOCKSPACE_ID, GUEST_RISC0_CHECKPOINT_ELF,
-        GUEST_RISC0_L1_BATCH_ELF, GUEST_RISC0_L1_BATCH_ID,
+    use express_sp1_adapter::{SP1Host, SP1ProofInputBuilder, SP1Verifier};
+    use express_sp1_guest_builder::{
+        GUEST_BTC_BLOCKSPACE_ELF, GUEST_CHECKPOINT_ELF, GUEST_L1_BATCH_ELF,
     };
     use express_zkvm::{
         AggregationInput, Proof, ProverOptions, VerificationKey, ZKVMHost, ZKVMInputBuilder,
@@ -24,7 +23,7 @@ mod test {
     };
 
     // TODO: handle this repeat
-    fn get_l1_batch_output_and_proof() -> (L1BatchProofOutput, Proof) {
+    fn get_l1_batch_output_and_proof() -> (L1BatchProofOutput, Proof, VerificationKey) {
         let mainnet_blocks: Vec<(u32, String)> = vec![
             (40321, "0100000045720d24eae33ade0d10397a2e02989edef834701b965a9b161e864500000000993239a44a83d5c427fd3d7902789ea1a4d66a37d5848c7477a7cf47c2b071cd7690784b5746651c3af7ca030101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff08045746651c02db00ffffffff0100f2052a01000000434104c9f513361104db6a84fb6d5b364ba57a27cd19bd051239bf750d8999c6b437220df8fea6b932a248df3cad1fdebb501791e02b7b893a44718d696542ba92a0acac00000000".to_owned()),
         ];
@@ -32,21 +31,16 @@ mod test {
         let prover_options = ProverOptions {
             use_mock_prover: false,
             stark_to_snark_conversion: false,
-            enable_compression: false,
+            enable_compression: true,
         };
-        let prover = RiscZeroHost::init(
-            GUEST_RISC0_BTC_BLOCKSPACE_ELF.into(),
+        let prover = SP1Host::init(
+            GUEST_BTC_BLOCKSPACE_ELF.into(),
             // Default::default(),
             prover_options,
         );
 
-        let btc_blockspace_elf_id: Vec<u8> = GUEST_RISC0_BTC_BLOCKSPACE_ID
-            .iter()
-            .flat_map(|&x| x.to_le_bytes())
-            .collect();
-
         let mut blockspace_outputs = Vec::new();
-        let mut prover_input = RiscZeroProofInputBuilder::new();
+        let mut prover_input = SP1ProofInputBuilder::new();
         for (_, raw_block) in mainnet_blocks {
             let block_bytes = hex::decode(&raw_block).unwrap();
             let scan_config = ScanRuleConfig {
@@ -57,7 +51,7 @@ mod test {
                 .assume_checked()
                 .script_pubkey()],
             };
-            let inner_prover_input = RiscZeroProofInputBuilder::new()
+            let inner_prover_input = SP1ProofInputBuilder::new()
                 .write(&scan_config)
                 .unwrap()
                 .write_serialized(&block_bytes)
@@ -65,37 +59,32 @@ mod test {
                 .build()
                 .unwrap();
 
-            let (proof, _) = prover
+            let (proof, vkey) = prover
                 .prove(inner_prover_input)
                 .expect("Failed to generate proof");
 
-            let output = Risc0Verifier::extract_public_output::<BlockspaceProofOutput>(&proof)
+            let output = SP1Verifier::extract_public_output::<BlockspaceProofOutput>(&proof)
                 .expect("Failed to extract public outputs");
 
-            prover_input
-                .write_proof(AggregationInput::new(
-                    proof,
-                    VerificationKey::new(btc_blockspace_elf_id.clone()),
-                ))
-                .unwrap();
+            let _ = prover_input.write_proof(AggregationInput::new(proof, vkey));
             blockspace_outputs.push(output);
         }
 
-        let prover = RiscZeroHost::init(GUEST_RISC0_L1_BATCH_ELF.into(), prover_options);
+        let prover = SP1Host::init(GUEST_L1_BATCH_ELF.into(), prover_options);
         let input = L1BatchProofInput {
             batch: blockspace_outputs,
             state: get_verification_state_for_block(40321, &PowParams::from(&MAINNET)),
         };
 
         let prover_input = prover_input.write(&input).unwrap().build().unwrap();
-        let (proof, _) = prover
+        let (proof, vk) = prover
             .prove(prover_input)
             .expect("Failed to generate proof");
 
-        let output = Risc0Verifier::extract_public_output::<L1BatchProofOutput>(&proof)
+        let output = SP1Verifier::extract_public_output::<L1BatchProofOutput>(&proof)
             .expect("Failed to extract public outputs");
 
-        (output, proof)
+        (output, proof, vk)
     }
 
     // fn get_l1_batch_output() -> L1BatchProofOutput {
@@ -120,8 +109,7 @@ mod test {
 
     #[test]
     fn test_checkpoint_proof() {
-        let (l1_batch, l1_batch_proof) = get_l1_batch_output_and_proof();
-        // let l1_batch = get_l1_batch_output();
+        let (l1_batch, l1_batch_proof, l1_batch_vk) = get_l1_batch_output_and_proof();
         let l2_batch = get_l2_batch_output();
 
         let genesis = HashedCheckpointState {
@@ -130,22 +118,15 @@ mod test {
         };
 
         let prover_options = ProverOptions {
-            use_mock_prover: false,
+            use_mock_prover: true,
             stark_to_snark_conversion: false,
             enable_compression: false,
         };
-        let prover = RiscZeroHost::init(GUEST_RISC0_CHECKPOINT_ELF.into(), prover_options);
+        let prover = SP1Host::init(GUEST_CHECKPOINT_ELF.into(), prover_options);
 
-        let l1_batch_image_id: Vec<u8> = GUEST_RISC0_L1_BATCH_ID
-            .iter()
-            .flat_map(|&x| x.to_le_bytes())
-            .collect();
-        let l1_batch_proof_input = AggregationInput::new(
-            l1_batch_proof,
-            VerificationKey::new(l1_batch_image_id.clone()),
-        );
+        let l1_batch_proof_input = AggregationInput::new(l1_batch_proof, l1_batch_vk);
 
-        let prover_input = RiscZeroProofInputBuilder::new()
+        let prover_input = SP1ProofInputBuilder::new()
             .write(&l1_batch)
             .unwrap()
             .write_serialized(&borsh::to_vec(&l2_batch).unwrap())
@@ -161,7 +142,7 @@ mod test {
             .prove(prover_input)
             .expect("Failed to generate proof");
 
-        let _output = Risc0Verifier::extract_public_output::<CheckpointProofOutput>(&proof)
+        let _output = SP1Verifier::extract_public_output::<CheckpointProofOutput>(&proof)
             .expect("Failed to extract public outputs");
     }
 }
