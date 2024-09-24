adc6f51a5f229d4942e16c8193c2478a586cf935
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-09-24 20:28:26 +0545

    wip(btc-blockspace): update guest programs

--- provers/sp1/guest-btc-blockspace/src/main.rs
+++ provers/sp1/guest-btc-blockspace/src/main.rs
@@ -1,12 +1,16 @@
+use alpen_express_btcio::reader::filter::TxFilterRule;
 use bitcoin::Block;
-use express_proofimpl_btc_blockspace::logic::{process_blockspace_proof, BlockspaceProofInput, ScanRuleConfig};
+use express_proofimpl_btc_blockspace::logic::{process_blockspace_proof, BlockspaceProofInput};
 
 fn main() {
-    let scan_config = sp1_zkvm::io::read::<ScanRuleConfig>();
+    let serialized_filters = sp1_zkvm::io::read_vec();
+    let filters: Vec<TxFilterRule> = borsh::from_slice(&serialized_filters).unwrap();
+
     let serialized_block = sp1_zkvm::io::read_vec();
     let block: Block = bitcoin::consensus::deserialize(&serialized_block).unwrap();
 
-    let input = BlockspaceProofInput { block, scan_config };
+    let input = BlockspaceProofInput { block, filters };
     let output = process_blockspace_proof(&input);
-    sp1_zkvm::io::commit(&output);
+
+    sp1_zkvm::io::commit(&borsh::to_vec(&output).unwrap());
 }
