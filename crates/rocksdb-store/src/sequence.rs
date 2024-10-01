0007371f5f869abb5ba03b14d3fa24482d966c3e
Sapin Bajracharya <sapin@alpenlabs.io>
2024-10-01 18:12:56 +0545

    refactor: remove duplication, use more readable names in sequence schema

--- crates/rocksdb-store/src/sequence.rs
+++ crates/rocksdb-store/src/sequence.rs
@@ -11,34 +11,28 @@ define_table_with_default_codec!(
 /// This should only be used to get atomically auto-incrementing keys for tables inside
 /// transactions.
 /// Should NEVER be updated directly outside of this method.
-/// Id starts from 0
+/// Id starts from 0, increments by 1
 pub(crate) fn get_next_id<S: Schema, DB: TransactionDBMarker>(
     txn: &TransactionCtx<DB>,
 ) -> anyhow::Result<u64> {
-    let sequence_key = S::COLUMN_FAMILY_NAME.as_bytes().to_vec();
-    let next_idx = txn
-        .get_for_update::<SequenceSchema>(&sequence_key)?
-        .map(|last_idx| last_idx + 1)
-        .unwrap_or(0);
-    txn.put::<SequenceSchema>(&sequence_key, &next_idx)?;
-    Ok(next_idx)
+    get_next_id_opts::<S, DB>(txn, |last_idx| last_idx + 1, 0)
 }
 
 /// Get next incremental id for given `Schema`.
 /// This should only be used to get atomically auto-incrementing keys for tables inside
 /// transactions.
 /// Should NEVER be updated directly outside of this method.
-/// index starts from `default` and updated by `increment(last_idx)`
+/// index starts from `starting_index` and updated by `update(last_idx)`
 pub(crate) fn get_next_id_opts<S: Schema, DB: TransactionDBMarker>(
     txn: &TransactionCtx<DB>,
-    increment: impl Fn(u64) -> u64,
-    default: u64,
+    update: impl Fn(u64) -> u64,
+    starting_index: u64,
 ) -> anyhow::Result<u64> {
     let index_key = S::COLUMN_FAMILY_NAME.as_bytes().to_vec();
     let next_idx = txn
         .get_for_update::<SequenceSchema>(&index_key)?
-        .map(increment)
-        .unwrap_or(default);
+        .map(update)
+        .unwrap_or(starting_index);
     txn.put::<SequenceSchema>(&index_key, &next_idx)?;
     Ok(next_idx)
 }
