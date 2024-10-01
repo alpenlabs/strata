8ca4d863dca906d4ac4fd81d87d3473440900a20
Trey Del Bonis <trey@alpenlabs.io>
2024-10-01 15:56:55 -0400

    common: tried to set up otlp
    This deletes Cargo.lock because we couldn't get the deps to work anyways.

--- crates/common/src/logging.rs
+++ crates/common/src/logging.rs
@@ -1,10 +1,51 @@
+use opentelemetry_otlp::WithExportConfig;
 use tracing::*;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-pub fn init() {
+pub struct LoggerConfig {
+    whoami: String,
+    otel_url: Option<String>,
+}
+
+impl LoggerConfig {
+    pub fn new(whoami: String) -> Self {
+        Self {
+            whoami,
+            otel_url: None,
+        }
+    }
+}
+
+pub fn init(config: LoggerConfig) {
     let filt = tracing_subscriber::EnvFilter::from_default_env();
-    tracing_subscriber::fmt()
+
+    let mut loggers: Vec<Box<dyn tracing::Subscriber>> = Vec::new();
+
+    // Stdout logging.
+    let stdout_sub = tracing_subscriber::fmt()
         .compact()
         .with_env_filter(filt)
-        .init();
-    info!("logging started");
+        .finish();
+    //loggers.push(Box::new(stdout_sub));
+
+    // OpenTelemetry output.
+    if let Some(otel_url) = &config.otel_url {
+        let exporter = opentelemetry_otlp::new_exporter()
+            .tonic()
+            .with_endpoint(otel_url);
+
+        let tracer = opentelemetry_otlp::new_pipeline()
+            .tracing()
+            .with_exporter(exporter)
+            .install_batch(opentelemetry_sdk::runtime::TokioCurrentThread)
+            .expect("init: opentelemetry");
+
+        // TODO
+
+        let otel_sub = tracing_opentelemetry::OpenTelemetryLayer::new(tracer);
+
+        //loggers.append(Box::new(stdout_sub));
+    }
+
+    info!(whoami = %config.whoami, "logging started");
 }
