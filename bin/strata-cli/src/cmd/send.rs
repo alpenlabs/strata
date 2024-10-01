29338ecb3959264d58795979e60c6d0a913d00be
Zk2u <azz@alpenlabs.io>
2024-10-01 17:31:20 +0100

    fix: use enum for network type in send
    also add note about sha2 dep on windows having issues with asm

--- bin/strata-cli/src/cmd/send.rs
+++ bin/strata-cli/src/cmd/send.rs
@@ -23,13 +23,9 @@ use crate::{
 #[derive(FromArgs, PartialEq, Debug)]
 #[argh(subcommand, name = "send")]
 pub struct SendArgs {
-    /// send via signet
-    #[argh(switch)]
-    signet: bool,
-
-    /// send via rollup
-    #[argh(switch)]
-    rollup: bool,
+    /// either "signet" or "rollup"
+    #[argh(positional)]
+    network_type: String,
 
     /// amount to send in sats
     #[argh(positional)]
@@ -40,55 +36,75 @@ pub struct SendArgs {
     address: String,
 }
 
+enum NetworkType {
+    Signet,
+    Rollup,
+}
+
+struct InvalidNetworkType;
+
+impl FromStr for NetworkType {
+    type Err = InvalidNetworkType;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "signet" => Ok(Self::Signet),
+            "rollup" => Ok(Self::Rollup),
+            _ => Err(InvalidNetworkType),
+        }
+    }
+}
+
 pub async fn send(args: SendArgs, seed: Seed, settings: Settings, esplora: EsploraClient) {
     let term = Term::stdout();
-    if args.signet && args.rollup {
-        let _ = term.write_line("Cannot use both --signet and --rollup options at once");
-        std::process::exit(1);
-    } else if !args.signet && !args.rollup {
-        let _ = term.write_line("Must specify either --signet and --rollup option");
-        std::process::exit(1);
-    }
+    let network_type = match NetworkType::from_str(&args.network_type) {
+        Ok(t) => t,
+        Err(InvalidNetworkType) => {
+            let _ = term.write_line("Invalid network type. Must be signet or rollup");
+            return;
+        }
+    };
 
-    let txid = if args.signet {
-        let amount = Amount::from_sat(args.amount);
-        let address = Address::from_str(&args.address)
-            .expect("valid address")
-            .require_network(NETWORK)
-            .expect("correct network");
-        let mut l1w = SignetWallet::new(&seed, NETWORK).expect("valid wallet");
-        l1w.sync(&esplora).await.unwrap();
-        let fee_rate = get_fee_rate(1, &esplora)
-            .await
-            .expect("valid response")
-            .expect("valid target");
-        let mut psbt = l1w
-            .build_tx()
-            .add_recipient(address.script_pubkey(), amount)
-            .enable_rbf()
-            .fee_rate(fee_rate)
-            .clone()
-            .finish()
-            .expect("valid psbt");
-        l1w.sign(&mut psbt, Default::default())
-            .expect("signable psbt");
-        let tx = psbt.extract_tx().expect("signed tx");
-        esplora.broadcast(&tx).await.expect("successful broadcast");
-        tx.compute_txid().as_raw_hash().to_byte_array()
-    } else if args.rollup {
-        let l2w = RollupWallet::new(&seed, &settings.l2_http_endpoint).expect("valid wallet");
-        let address = RollupAddress::from_str(&args.address).expect("valid address");
-        let tx = TransactionRequest::default()
-            .with_to(address)
-            // 1 btc == 1 "eth" => 1 sat = 1e10 "wei"
-            .with_value(U256::from(args.amount * 10u64.pow(10)));
-        l2w.send_transaction(tx)
-            .await
-            .expect("successful broadcast")
-            .tx_hash()
-            .0
-    } else {
-        unreachable!()
+    let txid = match network_type {
+        NetworkType::Signet => {
+            let amount = Amount::from_sat(args.amount);
+            let address = Address::from_str(&args.address)
+                .expect("valid address")
+                .require_network(NETWORK)
+                .expect("correct network");
+            let mut l1w = SignetWallet::new(&seed, NETWORK).expect("valid wallet");
+            l1w.sync(&esplora).await.unwrap();
+            let fee_rate = get_fee_rate(1, &esplora)
+                .await
+                .expect("valid response")
+                .expect("valid target");
+            let mut psbt = l1w
+                .build_tx()
+                .add_recipient(address.script_pubkey(), amount)
+                .enable_rbf()
+                .fee_rate(fee_rate)
+                .clone()
+                .finish()
+                .expect("valid psbt");
+            l1w.sign(&mut psbt, Default::default())
+                .expect("signable psbt");
+            let tx = psbt.extract_tx().expect("signed tx");
+            esplora.broadcast(&tx).await.expect("successful broadcast");
+            tx.compute_txid().as_raw_hash().to_byte_array()
+        }
+        NetworkType::Rollup => {
+            let l2w = RollupWallet::new(&seed, &settings.l2_http_endpoint).expect("valid wallet");
+            let address = RollupAddress::from_str(&args.address).expect("valid address");
+            let tx = TransactionRequest::default()
+                .with_to(address)
+                // 1 btc == 1 "eth" => 1 sat = 1e10 "wei"
+                .with_value(U256::from(args.amount * 10u64.pow(10)));
+            l2w.send_transaction(tx)
+                .await
+                .expect("successful broadcast")
+                .tx_hash()
+                .0
+        }
     };
 
     let _ = term.write_line(&format!(
