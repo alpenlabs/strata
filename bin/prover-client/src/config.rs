9c2cb81e19573b642ef692060dc407a7c35f9e81
abishek <abishek@alpenlabs.io>
2024-10-03 18:47:04 +0545

    prover manager l1 blocks -> l1 batch sp1

--- bin/prover-client/src/config.rs
+++ bin/prover-client/src/config.rs
@@ -1,5 +1,5 @@
 // Number of prover workers to spawn
-pub const NUM_PROVER_WORKERS: usize = 10;
+pub const NUM_PROVER_WORKERS: usize = 5;
 
 // Wait time in seconds for the prover manager loop
 pub const PROVER_MANAGER_INTERVAL: u64 = 1;
