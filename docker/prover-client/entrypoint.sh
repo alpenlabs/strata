f643e6b739cc30c79310292fcf567e7700b817f8
voidash <ashish.thapa477@gmail.com>
2024-10-07 12:53:58 +0545

    docker: add curl -fLsS option, and resolve comments for prover client

--- docker/prover-client/entrypoint.sh
+++ docker/prover-client/entrypoint.sh
@@ -1,13 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 # Exit on error
 set -e
 
 echo "starting Prover client"
 
-export RUST_LOG=trace
-
 strata-prover-client \
-    --rpc-port 9851\
-    --sequencer-rpc http://sequencer:8432\
+    --rpc-port 9851 \
+    --sequencer-rpc http://sequencer:8432 \
     --reth-rpc http://reth:8545 $@
