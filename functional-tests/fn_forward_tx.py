e0469ac925c8deaac599e3521877ad05fcc979de
Sapin Bajracharya <sapin@alpenlabs.io>
2024-10-07 10:55:53 +0545

    feat: use  tx forwarder to send raw tx from fullnodes to sequencer

---
+++ functional-tests/fn_forward_tx.py
@@ -0,0 +1,32 @@
+import time
+
+import flexitest
+from web3 import Web3
+
+
+@flexitest.register
+class FullnodeElBlockGenerationTest(flexitest.Test):
+    def __init__(self, ctx: flexitest.InitContext):
+        ctx.set_env("hub1")
+
+    def main(self, ctx: flexitest.RunContext):
+        seq_web3: Web3 = ctx.get_service("seq_reth").create_web3()
+        fn_web3: Web3 = ctx.get_service("follower_1_reth").create_web3()
+
+        # give some time for the sequencer to start up and generate blocks
+        time.sleep(3)
+
+        dest = fn_web3.to_checksum_address("deadf001900dca3ebeefdeadf001900dca3ebeef")
+
+        # send tx to fullnode reth
+        txid = fn_web3.eth.send_transaction(
+            {
+                "to": dest,
+                "value": hex(1_000_000_000),
+                "gas": hex(100000),
+                "from": fn_web3.address,
+            }
+        )
+
+        # expect receipt to be available from sequencer reth
+        seq_web3.eth.wait_for_transaction_receipt(txid, timeout=5)
