4c4b2cdd75cd2822189a4cd2e51636f812ed41d9
Zk2u <azz@aciedo.com>
2024-10-07 15:34:47 +0100

    chore: update bin/strata-cli/src/net_type.rs
    Co-authored-by: Jose Storopoli <jose@storopoli.io>

--- bin/strata-cli/src/net_type.rs
+++ bin/strata-cli/src/net_type.rs
@@ -21,7 +21,7 @@ impl FromStr for NetworkType {
     }
 }
 
-/// Parses `val` as a `NetworkType`. Prints error message and exits if invalid.
+/// Parses `val` as a [`NetworkType`]. Prints error message and exits if invalid.
 pub fn net_type_or_exit(val: &str, term: &Term) -> NetworkType {
     match NetworkType::from_str(val) {
         Ok(t) => t,
