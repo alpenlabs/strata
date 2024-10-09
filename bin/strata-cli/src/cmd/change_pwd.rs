e4fe8bc5979da907ce4e4d607318154124d00834
Jose Storopoli <jose@storopoli.io>
2024-10-09 16:36:37 -0300

    feat(strata-cli)!: add hash version and optional password (#392)
    * feat(strata-cli)!: add hash version and optional password
    
    * feat(strata-cli): warn user about password strength

--- bin/strata-cli/src/cmd/change_pwd.rs
+++ bin/strata-cli/src/cmd/change_pwd.rs
@@ -1,6 +1,7 @@
 use argh::FromArgs;
 use console::Term;
 use rand::thread_rng;
+use zxcvbn::Score;
 
 use crate::seed::{password::Password, EncryptedSeedPersister, Seed};
 
@@ -12,6 +13,17 @@ pub struct ChangePwdArgs {}
 pub async fn change_pwd(_args: ChangePwdArgs, seed: Seed, persister: impl EncryptedSeedPersister) {
     let term = Term::stdout();
     let mut new_pw = Password::read(true).unwrap();
+    let entropy = new_pw.entropy();
+    let _ = term.write_line(format!("Password strength (Overall strength score from 0-4, where anything below 3 is too weak): {}", entropy.score()).as_str());
+    if entropy.score() <= Score::Two {
+        let _ = term.write_line(
+            entropy
+                .feedback()
+                .expect("No feedback")
+                .to_string()
+                .as_str(),
+        );
+    }
     let encrypted_seed = seed.encrypt(&mut new_pw, &mut thread_rng()).unwrap();
     persister.save(&encrypted_seed).unwrap();
     let _ = term.write_line("Password changed successfully");
