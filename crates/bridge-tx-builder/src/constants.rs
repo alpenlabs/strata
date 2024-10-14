5bffae738125774c0ebe9f8158bc621b46e1a00e
Jose Storopoli <jose@storopoli.io>
2024-10-14 14:16:19 -0300

    fix(bridge-tx-builder): small fixes

--- crates/bridge-tx-builder/src/constants.rs
+++ crates/bridge-tx-builder/src/constants.rs
@@ -1,7 +1,5 @@
 //! Constants related to bridge transactions.
 
-use std::str::FromStr;
-
 use bitcoin::{secp256k1::XOnlyPublicKey, Amount};
 use strata_primitives::l1::BitcoinAmount;
 
@@ -30,5 +28,5 @@ lazy_static::lazy_static! {
     /// This is generated via <https://github.com/alpenlabs/unspendable-pubkey-gen> following [BIP 341](https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki#constructing-and-spending-taproot-outputs)
     /// with `r = 0x82758434e13488368e0781c4a94019d3d6722f854d26c15d2d157acd1f464723`.
     pub static ref UNSPENDABLE_INTERNAL_KEY: XOnlyPublicKey =
-        XOnlyPublicKey::from_str("2be4d02127fedf4c956f8e6d8248420b9af78746232315f72894f0b263c80e81").unwrap();
+        "2be4d02127fedf4c956f8e6d8248420b9af78746232315f72894f0b263c80e81".parse::<XOnlyPublicKey>().expect("should be a valid X-only public key");
 }
