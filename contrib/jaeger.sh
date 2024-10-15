2d7a8e21c20c7283830b5267c1eb03c3f0651cfb
Trey Del Bonis <trey@alpenlabs.io>
2024-10-15 13:57:53 -0400

    contrib: fixed Jaeger init script to use a recent version

--- contrib/jaeger.sh
+++ contrib/jaeger.sh
@@ -4,23 +4,25 @@
 #
 # Adapted from: https://www.jaegertracing.io/docs/1.6/getting-started/
 
+TAG_VER=1.62.0
+
 OTLP_GRPC_PORT=4317
 WEB_PORT=16686
 
 docker run -d \
-  --name jaeger \
-  -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
-  -e COLLECTOR_OTLP_ENABLED=true \
-  -p $OTLP_GRPC_PORT:4317 \
-  -p 4318:4318 \
-  -p 5775:5775/udp \
-  -p 6831:6831/udp \
-  -p 6832:6832/udp \
-  -p 5778:5778 \
-  -p $WEB_PORT:16686 \
-  -p 14268:14268 \
-  -p 9411:9411 \
-  jaegertracing/all-in-one:1.6.0
+       --name jaeger \
+       -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
+       -e COLLECTOR_OTLP_ENABLED=true \
+       -p $OTLP_GRPC_PORT:4317 \
+       -p 4318:4318 \
+       -p 5775:5775/udp \
+       -p 6831:6831/udp \
+       -p 6832:6832/udp \
+       -p 5778:5778 \
+       -p $WEB_PORT:16686 \
+       -p 14268:14268 \
+       -p 9411:9411 \
+       jaegertracing/all-in-one:$TAG_VER
 
-echo "Set 'export STRATA_OTLP_URL=http://127.0.0.1:$OTLP_GRPC_PORT'."
+echo "Set envvar: 'export STRATA_OTLP_URL=http://127.0.0.1:$OTLP_GRPC_PORT'"
 echo "Open http://localhost:$WEB_PORT/ in your browser."
