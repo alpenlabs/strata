88cfe82a22bcc3d4af4530c76d417dcd8e87fe88
abishek <abishek@alpenlabs.io>
2024-10-18 12:31:24 +0545

    sync witb feat/checkpoint-proof-verification

--- bin/prover-client/src/proving_ops/btc_ops.rs
+++ bin/prover-client/src/proving_ops/btc_ops.rs
@@ -1,17 +1,9 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use bitcoin::{key::Secp256k1, Block};
-use musig2::secp256k1::SecretKey;
-use rand::{rngs::StdRng, SeedableRng};
+use bitcoin::Block;
 use strata_btcio::rpc::{traits::Reader, BitcoinClient};
-use strata_primitives::{
-    block_credential::{self, CredRule},
-    buf::Buf32,
-    operator::OperatorPubkeys,
-    params::{OperatorConfig, Params, ProofPublishMode, RollupParams, SyncParams},
-    vk::RollupVerifyingKey,
-};
+use strata_primitives::{block_credential::CredRule, params::RollupParams};
 use strata_tx_parser::filter::{derive_tx_filter_rules, TxFilterRule};
 use tracing::debug;
 use uuid::Uuid;
@@ -67,75 +59,5 @@ impl ProvingOperations for BtcOperations {
         let prover_input = ZKVMInput::BtcBlock(block, cred_rule, tx_filters);
         let task_id = task_tracker.create_task(prover_input, vec![]).await;
         Ok(task_id)
-    }
-}
-
-// TODO: Move from manual param generation to importing params from the file
-pub fn get_pm_rollup_params() -> RollupParams {
-    // TODO: create a random seed if we really need random op_pubkeys every time this is called
-    gen_params_with_seed(0).rollup
-}
-
-fn gen_params_with_seed(seed: u64) -> Params {
-    let opkeys = make_dummy_operator_pubkeys_with_seed(seed);
-    Params {
-        rollup: RollupParams {
-            rollup_name: "strata".to_string(),
-            block_time: 60 * 1000,
-            cred_rule: block_credential::CredRule::Unchecked,
-            horizon_l1_height: 3,
-            genesis_l1_height: 496, // we have mainnet blocks from this height test-utils
-            operator_config: OperatorConfig::Static(vec![opkeys]),
-            evm_genesis_block_hash: Buf32(
-                "0x37ad61cff1367467a98cf7c54c4ac99e989f1fbb1bc1e646235e90c065c565ba"
-                    .parse()
-                    .unwrap(),
-            ),
-            evm_genesis_block_state_root: Buf32(
-                "0x351714af72d74259f45cd7eab0b04527cd40e74836a45abcae50f92d919d988f"
-                    .parse()
-                    .unwrap(),
-            ),
-            l1_reorg_safe_depth: 1,
-            target_l2_batch_size: 64,
-            address_length: 20,
-            deposit_amount: 1_000_000_000,
-            rollup_vk: RollupVerifyingKey::SP1VerifyingKey(Buf32(
-                "0x00cb1b9685add3fd4541c7623b5cd5c7823338c585c86725b20e9f73bdf4450a"
-                    .parse()
-                    .unwrap(),
-            )),
-            dispatch_assignment_dur: 64,
-            proof_publish_mode: ProofPublishMode::Strict,
-            max_deposits_in_block: 16,
-            network: bitcoin::Network::Regtest,
-        },
-        run: SyncParams {
-            l2_blocks_fetch_limit: 1000,
-            l1_follow_distance: 3,
-            client_checkpoint_interval: 10,
-        },
-    }
-}
-
-pub fn make_dummy_operator_pubkeys_with_seed(seed: u64) -> OperatorPubkeys {
-    let secp = Secp256k1::new();
-    let mut rng = StdRng::seed_from_u64(seed);
-    let sk = SecretKey::new(&mut rng);
-    let (pk, _) = sk.x_only_public_key(&secp);
-    OperatorPubkeys::new(pk.into(), pk.into())
-}
-
-#[cfg(test)]
-mod test {
-    use super::gen_params_with_seed;
-
-    #[test]
-    fn simple() {
-        let params = gen_params_with_seed(0);
-        println!("got the params {:#?}", params);
-
-        let params_hash = params.rollup().compute_hash();
-        println!("got the rollup params hash {:#?}", params_hash)
     }
 }
