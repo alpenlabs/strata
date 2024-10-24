e04d942c850dc305a82ebf3f81590435225198e0
Ashish Thapa <ashish.thapa477@gmail.com>
2024-10-24 16:51:50 +0545

    Proof dockerization (#427)
    * docker:add proof client
    
    * docker: add generate blk opt for bitcoind, custom chain for reth
    
    * docker: add curl -fLsS option, and resolve comments for prover client
    
    * Delete docker/script.sh

---
+++ docker/prover-client/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "starting Prover client"
+
+strata-prover-client \
+    --rpc-port 9851 \
+    --sequencer-rpc http://sequencer:8432 \
+    --reth-rpc http://reth:8545 $@
