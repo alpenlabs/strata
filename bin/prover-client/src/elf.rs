769c7e5319982b1dfb6d5bb32d681b887a44117d
abishek <abishek@alpenlabs.io>
2024-10-28 09:48:14 +0545

    prover client docker build fix

--- bin/prover-client/src/elf.rs
+++ bin/prover-client/src/elf.rs
@@ -1,3 +1,9 @@
+//! Mock ELF placeholders for the guest codes.
+//!
+//! These empty ELF constants are used when the prover client is built without the `prover-dev`
+//! feature, allowing builds without needing actual guest code. If `prover-dev` is enabled, the
+//! client will build and embed the actual ELF binaries.
+#![allow(unused)]
 pub const GUEST_EVM_EE_STF_ELF: &[u8] = &[];
 pub const GUEST_CL_STF_ELF: &[u8] = &[];
 pub const GUEST_CL_AGG_ELF: &[u8] = &[];
