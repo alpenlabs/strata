e207d52ed837c955d5e08dc76728359304fcf34f
Jose Storopoli <jose@storopoli.io>
2024-11-01 13:12:33 -0300

    python-utils: parse_outpoint

--- crates/util/python-utils/src/parse.rs
+++ crates/util/python-utils/src/parse.rs
@@ -1,4 +1,4 @@
-use bdk_wallet::bitcoin::{PublicKey, XOnlyPublicKey};
+use bdk_wallet::bitcoin::{OutPoint, PublicKey, Txid, XOnlyPublicKey};
 use reth_primitives::Address as RethAddress;
 
 use crate::error::Error;
@@ -23,6 +23,17 @@ pub(crate) fn parse_pk(pk: &str) -> Result<PublicKey, Error> {
     pk.parse::<PublicKey>().map_err(|_| Error::PublicKey)
 }
 
+/// Parse an [`OutPoint`] from a string.
+pub(crate) fn parse_outpoint(outpoint: &str) -> Result<OutPoint, Error> {
+    let parts: Vec<&str> = outpoint.split(':').collect();
+    if parts.len() != 2 {
+        return Err(Error::OutPoint);
+    }
+    let txid = parts[0].parse::<Txid>().map_err(|_| Error::OutPoint)?;
+    let vout = parts[1].parse::<u32>().map_err(|_| Error::OutPoint)?;
+    Ok(OutPoint { txid, vout })
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -44,5 +55,19 @@ mod tests {
     fn parse_pk() {
         let pk = "028b71ab391bc0a0f5fd8d136458e8a5bd1e035e27b8cef77b12d057b4767c31c8";
         assert!(super::parse_pk(pk).is_ok());
+    }
+
+    #[test]
+    fn parse_outpoint() {
+        let outpoint = "ae86b8c8912594427bf148eb7660a86378f2fb4ac9c8d2ea7d3cb7f3fcfd7c1c:0";
+        assert!(super::parse_outpoint(outpoint).is_ok());
+        let outpoint_without_vout =
+            "ae86b8c8912594427bf148eb7660a86378f2fb4ac9c8d2ea7d3cb7f3fcfd7c1c";
+        assert!(super::parse_outpoint(outpoint_without_vout).is_err());
+        let outpoint_with_vout_out_of_bonds = {
+            let vout = u32::MAX as u64 + 1;
+            format!("ae86b8c8912594427bf148eb7660a86378f2fb4ac9c8d2ea7d3cb7f3fcfd7c1c:{vout}")
+        };
+        assert!(super::parse_outpoint(&outpoint_with_vout_out_of_bonds).is_err());
     }
 }
