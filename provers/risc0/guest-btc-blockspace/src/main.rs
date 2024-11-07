e4cc173c68475c5e8797b60f84282f2e12d599bb
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-11-07 19:11:05 +0545

    refactor(risc0-guest): use zkvm trait

--- provers/risc0/guest-btc-blockspace/src/main.rs
+++ provers/risc0/guest-btc-blockspace/src/main.rs
@@ -1,21 +1,6 @@
-use bitcoin::{consensus::deserialize, Block};
-use strata_proofimpl_btc_blockspace::logic::{process_blockspace_proof, BlockspaceProofInput};
-use risc0_zkvm::guest::env;
-use strata_tx_parser::filter::TxFilterRule;
+use strata_proofimpl_btc_blockspace::logic::process_blockspace_proof_outer;
+use strata_risc0_adapter::ZkVmRisc0;
 
 fn main() {
-    let len: u32 = env::read();
-    let mut slice = vec![0u8; len as usize];
-    env::read_slice(&mut slice);
-    let filters: Vec<TxFilterRule> = borsh::from_slice(&slice).unwrap();
-
-    let len: u32 = env::read();
-    let mut slice = vec![0u8; len as usize];
-    env::read_slice(&mut slice);
-    let block: Block = deserialize(&slice).unwrap();
-
-    let input = BlockspaceProofInput { block, filters };
-    let output = process_blockspace_proof(&input);
-
-    env::commit(&borsh::to_vec(&output).unwrap());
+    process_blockspace_proof_outer(&ZkVmRisc0);
 }
