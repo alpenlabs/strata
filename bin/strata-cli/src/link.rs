9ccd3d7204ba1a442cc98611ccb6da4a1f1d92a5
Azz <azz@alpenlabs.io>
2024-11-27 13:54:35 +0000

    chore(cli) `link` module docstrings

--- bin/strata-cli/src/link.rs
+++ bin/strata-cli/src/link.rs
@@ -3,16 +3,40 @@ use std::fmt::Display;
 use alloy::primitives::{Address as StrataAddress, TxHash};
 use bdk_wallet::bitcoin;
 
+/// A thing that exists or is represented onchain.
+/// Currently, this is either a transaction or an address.
 pub enum OnchainObject<'a> {
     Transaction(Txid<'a>),
     Address(Address<'a>),
 }
 
-pub trait PrettyPrint {
-    fn pretty(&self) -> String;
+impl<'a> From<&'a bitcoin::Address> for OnchainObject<'a> {
+    fn from(address: &'a bitcoin::Address) -> Self {
+        OnchainObject::Address(Address::Bitcoin(address))
+    }
+}
+
+impl<'a> From<&'a StrataAddress> for OnchainObject<'a> {
+    fn from(address: &'a StrataAddress) -> Self {
+        OnchainObject::Address(Address::Strata(address))
+    }
+}
+
+impl<'a> From<&'a bitcoin::Txid> for OnchainObject<'a> {
+    fn from(txid: &'a bitcoin::Txid) -> Self {
+        OnchainObject::Transaction(Txid::Bitcoin(txid))
+    }
+}
+
+impl<'a> From<&'a TxHash> for OnchainObject<'a> {
+    fn from(txid: &'a TxHash) -> Self {
+        OnchainObject::Transaction(Txid::Strata(txid))
+    }
 }
 
 impl<'a> OnchainObject<'a> {
+    /// Create a link to the object on the given explorer domain.
+    /// Should be of the form `http{s}://{domain}`.
     pub fn with_explorer<'b>(self, explorer_domain: &'b str) -> Link<'a, 'b> {
         Link {
             object: self,
@@ -20,6 +44,12 @@ impl<'a> OnchainObject<'a> {
         }
     }
 
+    /// Create a link to the object on the given explorer domain, if it exists.
+    /// Should be of the form `http{s}://{domain}`.
+    ///
+    /// If `explorer_domain` is `None`, the object will be represented as-is.
+    ///
+    /// This is primarily a helper for displaying an OnChainObject in a user-facing context.
     pub fn with_maybe_explorer<'b>(self, explorer_domain: Option<&'b str>) -> MaybeLink<'a, 'b> {
         match explorer_domain {
             Some(dmn) => MaybeLink::Link(self.with_explorer(dmn)),
@@ -28,6 +58,16 @@ impl<'a> OnchainObject<'a> {
     }
 }
 
+/// A helper trait for pretty printing something into a human-readable string.
+/// Differs from `Display` in that it might add other wording outside of purely
+/// the string representation of `self`.
+///
+/// E.g. a transaction might be prefixed with "Transaction: " or "Tx: ".
+pub trait PrettyPrint {
+    fn pretty(&self) -> String;
+}
+
+/// A helper enum for pretty printing something that might be a link or an object.
 pub enum MaybeLink<'a, 'b> {
     Link(Link<'a, 'b>),
     Object(OnchainObject<'a>),
@@ -60,6 +100,7 @@ impl Display for OnchainObject<'_> {
     }
 }
 
+/// A wrapper around a bitcoin transaction id or a strata transaction id.
 #[derive(Clone, Copy)]
 pub enum Txid<'a> {
     Bitcoin(&'a bitcoin::Txid),
@@ -87,6 +128,7 @@ impl Display for Txid<'_> {
     }
 }
 
+/// A wrapper address that wraps either a bitcoin address or a strata address.
 #[derive(Clone, Copy)]
 pub enum Address<'a> {
     Bitcoin(&'a bitcoin::Address),
@@ -102,35 +144,15 @@ impl Display for Address<'_> {
     }
 }
 
+/// A link to an object on an explorer domain.
+///
+/// This is primarily a helper for displaying an OnChainObject in a user-facing
+/// context when an explorer domain is known.
 pub struct Link<'a, 'b> {
     /// Object of the link (Transaction or Address)
     object: OnchainObject<'a>,
     /// Domain of the explorer (will be used to build the URL)
     explorer_domain: &'b str,
-}
-
-impl<'a> From<&'a bitcoin::Address> for OnchainObject<'a> {
-    fn from(address: &'a bitcoin::Address) -> Self {
-        OnchainObject::Address(Address::Bitcoin(address))
-    }
-}
-
-impl<'a> From<&'a StrataAddress> for OnchainObject<'a> {
-    fn from(address: &'a StrataAddress) -> Self {
-        OnchainObject::Address(Address::Strata(address))
-    }
-}
-
-impl<'a> From<&'a bitcoin::Txid> for OnchainObject<'a> {
-    fn from(txid: &'a bitcoin::Txid) -> Self {
-        OnchainObject::Transaction(Txid::Bitcoin(txid))
-    }
-}
-
-impl<'a> From<&'a TxHash> for OnchainObject<'a> {
-    fn from(txid: &'a TxHash) -> Self {
-        OnchainObject::Transaction(Txid::Strata(txid))
-    }
 }
 
 impl<'b, 'a> Display for Link<'a, 'b> {
