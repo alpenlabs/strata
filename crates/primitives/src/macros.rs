954724faad6cdd95c50d30badf65661d5594fcb2
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2024-11-27 17:40:43 +0100

    Implement macro rule for buf wrapper with a common functionality.

--- crates/primitives/src/macros.rs
+++ crates/primitives/src/macros.rs
@@ -1,4 +1,41 @@
+#[macro_export]
+macro_rules! impl_buf_wrapper {
+    ($wrapper:ident, $name:ident, $len:expr) => {
+        impl ::std::convert::From<$name> for $wrapper {
+            fn from(value: $name) -> Self {
+                Self(value)
+            }
+        }
+
+        impl ::std::convert::From<$wrapper> for $name {
+            fn from(value: $wrapper) -> Self {
+                value.0
+            }
+        }
+
+        impl ::std::convert::AsRef<[u8; $len]> for $wrapper {
+            fn as_ref(&self) -> &[u8; $len] {
+                self.0.as_ref()
+            }
+        }
+
+        impl ::core::fmt::Debug for $wrapper {
+            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                ::core::fmt::Debug::fmt(&self.0, f)
+            }
+        }
+
+        impl ::core::fmt::Display for $wrapper {
+            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                ::core::fmt::Display::fmt(&self.0, f)
+            }
+        }
+    };
+}
+
 pub mod internal {
+    // Crate-internal impls.
+
     macro_rules! impl_buf_common {
         ($name:ident, $len:expr) => {
             impl $name {
--- crates/state/src/id.rs
+++ crates/state/src/id.rs
@@ -1,9 +1,7 @@
-use std::fmt;
-
 use arbitrary::Arbitrary;
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
-use strata_primitives::buf::Buf32;
+use strata_primitives::{buf::Buf32, impl_buf_wrapper};
 
 /// ID of an L2 block, usually the hash of its root header.
 #[derive(
@@ -23,32 +21,4 @@ use strata_primitives::buf::Buf32;
 )]
 pub struct L2BlockId(Buf32);
 
-impl From<Buf32> for L2BlockId {
-    fn from(value: Buf32) -> Self {
-        Self(value)
-    }
-}
-
-impl From<L2BlockId> for Buf32 {
-    fn from(value: L2BlockId) -> Self {
-        value.0
-    }
-}
-
-impl AsRef<[u8; 32]> for L2BlockId {
-    fn as_ref(&self) -> &[u8; 32] {
-        self.0.as_ref()
-    }
-}
-
-impl fmt::Debug for L2BlockId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Debug::fmt(&self.0, f)
-    }
-}
-
-impl fmt::Display for L2BlockId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(&self.0, f)
-    }
-}
+impl_buf_wrapper!(L2BlockId, Buf32, 32);
