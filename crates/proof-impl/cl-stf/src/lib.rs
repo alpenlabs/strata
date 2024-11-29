2f4e6407657454ef690101b576154125432247ad
Trey Del Bonis <trey@alpenlabs.io>
2024-11-29 16:57:42 -0500

    misc: fixed a bunch more unit tests

--- crates/proof-impl/cl-stf/src/lib.rs
+++ crates/proof-impl/cl-stf/src/lib.rs
@@ -17,7 +17,7 @@ pub use strata_state::{block::L2Block, chain_state::Chainstate, state_op::StateC
 use strata_zkvm::ZkVmEnv;
 
 #[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
-pub struct ChainStateSnapshot {
+pub struct ChainstateSnapshot {
     pub hash: Buf32,
     pub slot: u64,
     pub l2_blockid: L2BlockId,
@@ -26,8 +26,8 @@ pub struct ChainStateSnapshot {
 #[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
 pub struct L2BatchProofOutput {
     pub deposits: Vec<DepositInfo>,
-    pub initial_snapshot: ChainStateSnapshot,
-    pub final_snapshot: ChainStateSnapshot,
+    pub initial_snapshot: ChainstateSnapshot,
+    pub final_snapshot: ChainstateSnapshot,
     pub rollup_params_commitment: Buf32,
 }
 
@@ -123,13 +123,13 @@ pub fn process_cl_stf(zkvm: &impl ZkVmEnv, el_vkey: &[u32; 8]) {
         &rollup_params,
     );
 
-    let initial_snapshot = ChainStateSnapshot {
+    let initial_snapshot = ChainstateSnapshot {
         hash: prev_state.compute_state_root(),
         slot: prev_state.chain_tip_slot(),
         l2_blockid: prev_state.chain_tip_blockid(),
     };
 
-    let final_snapshot = ChainStateSnapshot {
+    let final_snapshot = ChainstateSnapshot {
         hash: new_state.compute_state_root(),
         slot: new_state.chain_tip_slot(),
         l2_blockid: new_state.chain_tip_blockid(),
