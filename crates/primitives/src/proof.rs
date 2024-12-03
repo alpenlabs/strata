94532252f5c5005062c1e7c9faeba2eda38a3c8d
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-12-03 16:11:43 +0545

    feat: define ProofId

--- crates/primitives/src/proof.rs
+++ crates/primitives/src/proof.rs
@@ -1,3 +1,4 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
 
 use crate::buf::Buf32;
@@ -9,4 +10,29 @@ pub enum RollupVerifyingKey {
     SP1VerifyingKey(Buf32),
     // Verifying Key used to verify proof created using Risc0
     Risc0VerifyingKey(Buf32),
+}
+
+/// `ProofId` is an enumeration representing various identifiers for proofs.
+/// It is used to uniquely identify the proofing tasks for different types of proofs.
+#[derive(
+    Debug, Clone, Copy, PartialEq, BorshSerialize, BorshDeserialize, Serialize, Deserialize,
+)]
+pub enum ProofId {
+    /// Represents the height of the L1 block whose blockspace proof is being prepared.
+    BtcBlockspace(u64),
+
+    /// Represents a range of L1 blocks (inclusive) that are being proven as part of a batch.
+    /// The first `u64` is the starting height, and the second `u64` is the ending height.
+    L1Batch(u64, u64),
+
+    /// Represents the height of the EVM Execution Environment (EE) block for which
+    /// the State Transition Function (STF) proof is being generated.
+    EvmEeStf(u64),
+
+    /// Represents the height of the Consensus Layer (CL) block for which
+    /// the State Transition Function (STF) proof is being generated.
+    ClStf(u64),
+
+    /// Represents the index of the checkpoint that is being proven.
+    Checkpoint(u64),
 }
