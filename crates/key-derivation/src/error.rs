50a956c1952b2d9ede4f7b61000cdcbcfc9e4835
Jose Storopoli <jose@storopoli.io>
2024-12-05 10:42:50 -0300

    errors: better imports and impls
    Signed-off-by: Jose Storopoli <jose@storopoli.io>

--- crates/key-derivation/src/error.rs
+++ crates/key-derivation/src/error.rs
@@ -1,22 +1,23 @@
 //! Error types for key derivation.
 
-use std::fmt::Display;
+use std::fmt::{Debug, Display, Formatter, Result};
 
+use bitcoin::bip32;
 use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
 pub enum KeyError {
-    /// An error from the [`bitcoin::bip32`] module.
+    /// An error from the [`bip32`] module.
     ///
-    /// This means that the [`Xpriv`](bitcoin::bip32::Xpriv)
+    /// This means that the [`Xpriv`](bip32::Xpriv)
     /// is not a valid extended private key or the
-    /// [`DerivationPath`](bitcoin::bip32::DerivationPath)
+    /// [`DerivationPath`](bip32::DerivationPath)
     /// is invalid.
-    Bip32Error(#[from] bitcoin::bip32::Error),
+    Bip32Error(#[from] bip32::Error),
 }
 
 impl Display for KeyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Debug::fmt(self, f)
     }
 }
