3491e98b76adf40a094a9dde3b604c0a4b194628
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-12-05 18:12:37 +0545

    feat: add handler for btc and l1 batch

---
+++ bin/prover-client/src/handlers/mod.rs
@@ -0,0 +1,18 @@
+use strata_primitives::proof::ProofKey;
+use strata_rocksdb::prover::db::ProverDB;
+use strata_zkvm::ZkVmProver;
+
+use crate::errors::ProvingTaskError;
+
+pub mod btc;
+pub mod l1_batch;
+
+pub trait ProofHandler {
+    type Prover: ZkVmProver;
+
+    async fn fetch_input(
+        &self,
+        task_id: &ProofKey,
+        db: &ProverDB,
+    ) -> Result<<Self::Prover as ZkVmProver>::Input, ProvingTaskError>;
+}
