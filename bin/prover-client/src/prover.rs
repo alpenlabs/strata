d2477e6f8ba999a1f4f7034af1db47fd706598fa
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-12-05 15:12:55 +0545

    fix: updates to make things work

--- bin/prover-client/src/prover.rs
+++ bin/prover-client/src/prover.rs
@@ -10,8 +10,6 @@ use strata_rocksdb::{
     prover::db::{ProofDb, ProverDB},
     DbOpsConfig,
 };
-use strata_sp1_adapter::SP1Host;
-use strata_sp1_guest_builder::*;
 use strata_state::l1::L1BlockId;
 use strata_zkvm::{Proof, ProofReceipt, ProofType, ZkVmHost, ZkVmInputBuilder};
 use tracing::{error, info};
@@ -93,7 +91,7 @@ pub(crate) struct Prover {
     prover_state: Arc<RwLock<ProverState>>,
     db: ProverDB,
     pool: rayon::ThreadPool,
-    vm_manager: ZkVMManager<SP1Host>, // TODO: make this generic
+    vm_manager: ZkVMManager, // TODO: make this generic
 }
 
 fn make_proof<Vm>(zkvm_input: ZkVmInput, vm: &'static Vm) -> Result<ProofWithVkey, anyhow::Error>
