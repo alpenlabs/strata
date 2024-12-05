edb9acf2296f71552f960b51299a227346808ef4
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-12-05 20:08:14 +0545

    refactor: rename zkvms to host

--- bin/prover-client/src/primitives/vms.rs
+++ bin/prover-client/src/primitives/vms.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, hash::Hash};
 
 use strata_sp1_adapter::SP1Host;
 
-use crate::zkvm;
+use crate::hosts;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ProofVm {
@@ -28,7 +28,7 @@ impl ZkVMManager {
     pub fn add_vm(&mut self, proof_vm: ProofVm) {
         // The `Vm` is expected to live for the lifetime of the ProverManager, ensuring the same
         // instance is reused to prove the same guest program
-        let vm = zkvm::get_host(proof_vm);
+        let vm = hosts::get_host(proof_vm);
         self.vms.insert(proof_vm, vm);
     }
 
