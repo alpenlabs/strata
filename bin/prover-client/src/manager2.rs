93c1e7e524733db5ae46715c8fa3cbc96f53e7bb
Prajwol Gyawali <prajwol@alpenlabs.io>
2024-12-06 12:16:20 +0545

    feat: update manager with proving

--- bin/prover-client/src/manager2.rs
+++ bin/prover-client/src/manager2.rs
@@ -2,13 +2,17 @@ use std::sync::Arc;
 
 use jsonrpsee::http_client::HttpClient;
 use strata_btcio::rpc::BitcoinClient;
+use strata_db::traits::ProverDatabase;
+use strata_primitives::proof::ProofKey;
 use strata_rocksdb::{
     prover::db::{ProofDb, ProverDB},
     DbOpsConfig,
 };
+use tokio::sync::Mutex;
 
 use crate::{
     db::open_rocksdb_database,
+    errors::ProvingTaskError,
     handlers::{
         btc::BtcBlockspaceHandler, checkpoint::CheckpointHandler, cl_agg::ClAggHandler,
         cl_stf::ClStfHandler, evm_ee::EvmEeHandler, l1_batch::L1BatchHandler, ProofHandler,
@@ -18,9 +22,10 @@ use crate::{
 };
 
 pub struct ProverManager {
-    task_tracker: TaskTracker,
+    task_tracker: Arc<Mutex<TaskTracker>>,
     db: ProverDB,
     handler: ProofHandler,
+    workers: usize,
 }
 
 impl ProverManager {
@@ -28,6 +33,7 @@ impl ProverManager {
         btc_client: BitcoinClient,
         evm_ee_client: HttpClient,
         cl_client: HttpClient,
+        workers: usize,
     ) -> Self {
         let rbdb = open_rocksdb_database().unwrap();
         let db_ops = DbOpsConfig { retry_count: 3 };
@@ -55,23 +61,57 @@ impl ProverManager {
             checkpoint_handler,
         );
 
-        let task_tracker = TaskTracker::new();
+        let task_tracker = Arc::new(Mutex::new(TaskTracker::new()));
         Self {
             task_tracker,
             db: ProverDB::new(Arc::new(db)),
             handler,
+            workers,
         }
     }
 
-    pub async fn process_pending_tasks(&mut self) {
-        let pending_tasks = self
-            .task_tracker
-            .get_tasks_by_status(|status| matches!(status, ProvingTaskStatus::Pending));
+    pub async fn process_pending_tasks(&self) {
+        // Acquire lock to get pending tasks
+        let pending_tasks = {
+            let task_tracker = self.task_tracker.lock().await;
+            task_tracker.get_tasks_by_status(|status| matches!(status, ProvingTaskStatus::Pending))
+        };
 
+        // Now iterate without holding the lock
         for task in pending_tasks {
-            self.handler
-                .prove(&mut self.task_tracker, &task, &self.db)
-                .await;
+            {
+                let task_tracker = self.task_tracker.lock().await;
+                if task_tracker.in_progress_tasks_count() >= self.workers {
+                    break; // No need to spawn more
+                }
+            }
+
+            let handler = self.handler.clone();
+            let db = self.db.proof_db().clone();
+            let task_tracker = self.task_tracker.clone();
+            tokio::spawn(async move { make_proof(handler, task_tracker, task, db).await });
         }
     }
+}
+
+pub async fn make_proof(
+    handler: ProofHandler,
+    task_tracker: Arc<Mutex<TaskTracker>>,
+    task: ProofKey,
+    db: Arc<ProofDb>,
+) -> Result<(), ProvingTaskError> {
+    {
+        let mut task_tracker = task_tracker.lock().await;
+        task_tracker.update_status(task, ProvingTaskStatus::ProvingInProgress)?;
+    }
+
+    let _ = handler.prove(&task, &db).await;
+    // TODO: handle different errors for different failure condition
+
+    {
+        let mut task_tracker = task_tracker.lock().await;
+        task_tracker.update_status(task, ProvingTaskStatus::Completed)?;
+    }
+
+    Ok(())
 }
