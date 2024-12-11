1deafdf338f8347e5d1a51b4e94dfd84b8260020
Purushotam <purushotam@alpenlabs.io>
2024-12-11 11:11:28 +0545

    code refactor and docs

--- crates/test-utils/src/lib.rs
+++ crates/test-utils/src/lib.rs
@@ -6,8 +6,8 @@ pub mod bridge;
 pub mod evm_ee;
 pub mod l2;
 
-// Smaller buffer size as compared to 2^24
-const ARB_GEN_LEN: usize = 128;
+/// The default buffer size for the `ArbitraryGenerator`.
+const ARB_GEN_LEN: usize = 1024;
 
 pub struct ArbitraryGenerator {
     buf: Vec<u8>, // Persistent buffer
@@ -24,9 +24,7 @@ impl ArbitraryGenerator {
     ///
     /// A new instance of `ArbitraryGenerator`.
     pub fn new() -> Self {
-        ArbitraryGenerator {
-            buf: vec![0u8; ARB_GEN_LEN],
-        }
+        Self::new_with_size(ARB_GEN_LEN)
     }
 
     /// Creates a new `ArbitraryGenerator` with a specified buffer size.
@@ -39,7 +37,7 @@ impl ArbitraryGenerator {
     ///
     /// A new instance of `ArbitraryGenerator` with the specified buffer size.
     pub fn new_with_size(s: usize) -> Self {
-        ArbitraryGenerator { buf: vec![0u8; s] }
+        Self { buf: vec![0u8; s] }
     }
 
     /// Generates an arbitrary instance of type `T` using the default RNG, [`OsRng`].
@@ -47,43 +45,28 @@ impl ArbitraryGenerator {
     /// # Returns
     ///
     /// An arbitrary instance of type `T`.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if it fails to generate an arbitrary instance.
     pub fn generate<'a, T>(&'a mut self) -> T
     where
         T: Arbitrary<'a> + Clone,
     {
-        self.generate_with_rng::<T, OsRng>(None)
+        self.generate_with_rng::<T, OsRng>(&mut OsRng)
     }
 
-    /// Generates an arbitrary instance of type `T` using an optional RNG.
+    /// Generates an arbitrary instance of type `T`.
     ///
     /// # Arguments
     ///
-    /// * `rng` - An optional RNG to be used for generating the arbitrary instance. If `None`, the
-    ///   default RNG, [`OsRng`], will be used. The provided RNG must implement the [`RngCore`] and
-    ///   [`CryptoRng`] traits.
+    /// * `rng` - An RNG to be used for generating the arbitrary instance. Provided RNG must
+    ///   implement the [`RngCore`] and [`CryptoRng`] traits.
     ///
     /// # Returns
     ///
     /// An arbitrary instance of type `T`.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if it fails to generate an arbitrary instance.
-    pub fn generate_with_rng<'a, T, R>(&'a mut self, rng: Option<&mut R>) -> T
+    pub fn generate_with_rng<'a, T, R>(&'a mut self, rng: &mut R) -> T
     where
         T: Arbitrary<'a> + Clone,
         R: RngCore + CryptoRng,
     {
-        let mut thread_rng = OsRng; // Default secure RNG
-        let rng: &mut dyn RngCore = match rng {
-            Some(r) => r,
-            None => &mut thread_rng as &mut dyn RngCore,
-        };
-
         rng.fill_bytes(&mut self.buf);
         let mut u = Unstructured::new(&self.buf);
         T::arbitrary(&mut u).expect("Failed to generate arbitrary instance")
