afaa63847b349afd78671b673a04e6d5e4396059
Jose Storopoli <jose@storopoli.com>
2024-12-12 08:56:57 -0300

    bridge-client: zeroize again the buffer and the xpriv

--- bin/bridge-client/src/xpriv.rs
+++ bin/bridge-client/src/xpriv.rs
@@ -4,21 +4,38 @@ use std::{
     env,
     fs::read_to_string,
     path::{Path, PathBuf},
-    str::FromStr,
 };
 
 use bitcoin::bip32::Xpriv;
 use strata_key_derivation::operator::OperatorKeys;
 use strata_primitives::keys::ZeroizableXpriv;
+use zeroize::Zeroize;
 
 /// The environment variable that contains the operator's master [`Xpriv`].
 const OPXPRIV_ENVVAR: &str = "STRATA_OP_MASTER_XPRIV";
 
 /// Parses the master [`Xpriv`] from a file.
 pub(crate) fn parse_master_xpriv(path: &Path) -> anyhow::Result<OperatorKeys> {
-    let xpriv: ZeroizableXpriv = Xpriv::from_str(read_to_string(path)?.as_ref())?.into();
+    let mut xpriv_str = read_to_string(path)?;
+    match xpriv_str.parse::<Xpriv>() {
+        Ok(mut xpriv) => {
+            // Zeroize the xpriv string after parsing it.
+            xpriv_str.zeroize();
 
-    OperatorKeys::new(&xpriv).map_err(|_| anyhow::anyhow!("invalid master xpriv"))
+            // Parse into ZeroizableXpriv
+            let zeroizable_xpriv: ZeroizableXpriv = xpriv.into();
+
+            // Zeroize the xpriv after parsing it.
+            xpriv.private_key.non_secure_erase();
+
+            // Finally return the operator keys
+            //
+            // NOTE: `zeroizable_xpriv` is zeroized on drop.
+            Ok(OperatorKeys::new(&zeroizable_xpriv)
+                .map_err(|_| anyhow::anyhow!("invalid master xpriv"))?)
+        }
+        Err(e) => anyhow::bail!("invalid master xpriv: {}", e),
+    }
 }
 
 /// Resolves the master [`Xpriv`] from ENV vars or CLI.
