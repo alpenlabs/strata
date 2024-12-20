0dc2f92bac777cee049ad5c18f75a53cea7ab05b
Sapin Bajracharya <sapin@alpenlabs.io>
2024-12-20 14:46:17 +0545

    add block template manager, handles

---
+++ crates/block-assembly/src/handle.rs
@@ -0,0 +1,79 @@
+use strata_primitives::l2::L2BlockId;
+use strata_state::block::L2BlockBundle;
+use tokio::sync::{mpsc, oneshot};
+
+use crate::{BlockCompletionData, BlockGenerationConfig, BlockTemplate, Error};
+
+pub enum TemplateManagerRequest {
+    GenerateBlockTemplate(
+        BlockGenerationConfig,
+        oneshot::Sender<Result<BlockTemplate, Error>>,
+    ),
+    CompleteBlockTemplate(
+        L2BlockId,
+        BlockCompletionData,
+        oneshot::Sender<Result<L2BlockBundle, Error>>,
+    ),
+    GetBlockTemplate(L2BlockId, oneshot::Sender<Result<BlockTemplate, Error>>),
+}
+
+#[derive(Debug, Clone)]
+pub struct TemplateManagerHandle {
+    tx: mpsc::Sender<TemplateManagerRequest>,
+}
+
+impl TemplateManagerHandle {
+    pub fn new(tx: mpsc::Sender<TemplateManagerRequest>) -> Self {
+        Self { tx }
+    }
+
+    pub async fn generate_block_template(
+        &self,
+        config: BlockGenerationConfig,
+    ) -> Result<BlockTemplate, Error> {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send(TemplateManagerRequest::GenerateBlockTemplate(config, tx))
+            .await
+            .map_err(|_| Error::ChannelError("send"))?;
+
+        match rx.await {
+            Err(_) => Err(Error::ChannelError("oneshot tx dropped")),
+            Ok(res) => res,
+        }
+    }
+
+    pub async fn complete_block_template(
+        &self,
+        template_id: L2BlockId,
+        completion: BlockCompletionData,
+    ) -> Result<L2BlockBundle, Error> {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send(TemplateManagerRequest::CompleteBlockTemplate(
+                template_id,
+                completion,
+                tx,
+            ))
+            .await
+            .map_err(|_| Error::ChannelError("send"))?;
+
+        match rx.await {
+            Err(_) => Err(Error::ChannelError("oneshot tx dropped")),
+            Ok(res) => res,
+        }
+    }
+
+    pub async fn get_block_template(&self, template_id: L2BlockId) -> Result<BlockTemplate, Error> {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send(TemplateManagerRequest::GetBlockTemplate(template_id, tx))
+            .await
+            .map_err(|_| Error::ChannelError("send"))?;
+
+        match rx.await {
+            Err(_) => Err(Error::ChannelError("oneshot tx dropped")),
+            Ok(res) => res,
+        }
+    }
+}
---
+++ crates/block-assembly/src/worker.rs
@@ -0,0 +1,105 @@
+use std::time;
+
+use strata_consensus_logic::duty::block_assembly::prepare_block;
+use strata_db::traits::{Database, L2BlockDatabase};
+use strata_eectl::engine::ExecEngineCtl;
+use strata_primitives::params::Params;
+use strata_state::header::L2Header;
+use strata_status::StatusChannel;
+use tokio::sync::mpsc;
+
+use crate::{
+    BlockGenerationConfig, BlockTemplate, BlockTemplateFull, BlockTemplateManager, Error,
+    TemplateManagerRequest,
+};
+
+fn now_millis() -> u64 {
+    time::UNIX_EPOCH.elapsed().unwrap().as_millis() as u64
+}
+
+pub async fn template_manager_worker<D, E>(
+    mut manager: BlockTemplateManager<D, E>,
+    mut rx: mpsc::Receiver<TemplateManagerRequest>,
+) -> anyhow::Result<()>
+where
+    D: Database,
+    E: ExecEngineCtl,
+{
+    while let Some(request) = rx.recv().await {
+        match request {
+            TemplateManagerRequest::GenerateBlockTemplate(config, sender) => {
+                let _ = sender.send(generate_block_template(&mut manager, config));
+            }
+            TemplateManagerRequest::CompleteBlockTemplate(template_id, completion, sender) => {
+                let _ = sender.send(manager.complete_block_template(template_id, completion));
+            }
+            TemplateManagerRequest::GetBlockTemplate(block_id, sender) => {
+                let _ = sender.send(manager.get_block_template(block_id));
+            }
+        };
+    }
+
+    Ok(())
+}
+
+fn generate_block_template<D, E>(
+    manager: &mut BlockTemplateManager<D, E>,
+    config: BlockGenerationConfig,
+) -> Result<BlockTemplate, Error>
+where
+    D: Database,
+    E: ExecEngineCtl,
+{
+    let full_template = generate_block_template_inner(
+        config,
+        manager.params.as_ref(),
+        manager.database.as_ref(),
+        manager.engine.as_ref(),
+        &manager.status_channel,
+    )?;
+
+    let template = BlockTemplate::from_full_ref(&full_template);
+
+    let block_id = full_template.block_id();
+
+    manager.insert_template(block_id, full_template);
+
+    Ok(template)
+}
+
+fn generate_block_template_inner<D: Database, E: ExecEngineCtl>(
+    config: BlockGenerationConfig,
+    params: &Params,
+    database: &D,
+    engine: &E,
+    status_channel: &StatusChannel,
+) -> Result<BlockTemplateFull, Error> {
+    // get parent block
+    let parent_block_id = config.parent_block_id();
+    let l2db = database.l2_db();
+    let parent = l2db
+        .get_block_data(parent_block_id)?
+        .ok_or(Error::UnknownBlockId(parent_block_id))?;
+
+    // next slot idx
+    let slot = parent.header().blockidx() + 1;
+
+    // next block timestamp
+    let ts = config.ts(now_millis());
+
+    // latest l1 view from client state
+    let l1_state = status_channel.l1_view();
+
+    let (header, body, accessory) = prepare_block(
+        slot,
+        parent_block_id,
+        parent,
+        &l1_state,
+        ts,
+        database,
+        engine,
+        params,
+    )?;
+
+    Ok(BlockTemplateFull::new(header, body, accessory))
+}
