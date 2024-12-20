7e5b6fe099200abda64600011cce9ce2b8b2f790
Sapin Bajracharya <sapin@alpenlabs.io>
2024-12-20 14:46:17 +0545

    add block template manager, handles

---
+++ crates/block-assembly/src/error.rs
@@ -0,0 +1,17 @@
+use strata_db::DbError;
+use strata_primitives::{buf::Buf64, l2::L2BlockId};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("unknown blockId: {0}")]
+    UnknownBlockId(L2BlockId),
+    #[error("invalid signature: {0}, {1}")]
+    InvalidSignature(L2BlockId, Buf64),
+    #[error("{0}")]
+    DbError(#[from] DbError),
+    #[error("{0}")]
+    ConsensusError(#[from] strata_consensus_logic::errors::Error),
+    #[error("channel: {0}")]
+    ChannelError(&'static str),
+}
