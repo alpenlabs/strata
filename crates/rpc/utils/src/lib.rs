0e4ee4d91c32f79742da35ef028e3dfb84648467
Sapin Bajracharya <sapin@alpenlabs.io>
2024-12-20 14:46:56 +0545

    util: optional err object

--- crates/rpc/utils/src/lib.rs
+++ crates/rpc/utils/src/lib.rs
@@ -1,13 +1,13 @@
 use jsonrpsee::types::ErrorObjectOwned;
 
-pub fn to_jsonrpsee_error_object(err: impl ToString, message: &str) -> ErrorObjectOwned {
+pub fn to_jsonrpsee_error_object(err: Option<impl ToString>, message: &str) -> ErrorObjectOwned {
     ErrorObjectOwned::owned(
         jsonrpsee::types::error::UNKNOWN_ERROR_CODE,
         message,
-        Some(err.to_string()),
+        err.map(|e| e.to_string()),
     )
 }
 
 pub fn to_jsonrpsee_error<T: ToString>(message: &'static str) -> impl Fn(T) -> ErrorObjectOwned {
-    move |err: T| to_jsonrpsee_error_object(err, message)
+    move |err: T| to_jsonrpsee_error_object(Some(err), message)
 }
