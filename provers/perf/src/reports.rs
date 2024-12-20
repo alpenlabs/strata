9a248cfbc4cf32f0e4f19f2bc4d016c26a5fe376
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2024-12-20 18:09:32 +0100

    Temporary disable risc0, requires installation of its toolchain.

--- provers/perf/src/reports.rs
+++ provers/perf/src/reports.rs
@@ -1,6 +1,6 @@
 use sp1_prover::utils::get_cycles;
 use strata_native_zkvm_adapter::NativeHost;
-use strata_risc0_adapter::Risc0Host;
+//use strata_risc0_adapter::Risc0Host;
 use strata_sp1_adapter::SP1Host;
 
 use crate::{ProofReport, ZkVmHostPerf};
@@ -17,15 +17,15 @@ impl ZkVmHostPerf for SP1Host {
     }
 }
 
-impl ZkVmHostPerf for Risc0Host {
-    fn perf_report<'a>(
-        &self,
-        _input: <Self::Input<'a> as strata_zkvm::ZkVmInputBuilder<'a>>::Input,
-        _proof_type: strata_zkvm::ProofType,
-    ) -> strata_zkvm::ZkVmResult<ProofReport> {
-        Ok(ProofReport { cycles: 0 })
-    }
-}
+//impl ZkVmHostPerf for Risc0Host {
+//    fn perf_report<'a>(
+//        &self,
+//        _input: <Self::Input<'a> as strata_zkvm::ZkVmInputBuilder<'a>>::Input,
+//        _proof_type: strata_zkvm::ProofType,
+//    ) -> strata_zkvm::ZkVmResult<ProofReport> {
+//        Ok(ProofReport { cycles: 0 })
+//    }
+//}
 
 impl ZkVmHostPerf for NativeHost {
     fn perf_report<'a>(
