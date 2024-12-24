3230d443e0957fb377dbb70c875a8f0cf5286390
voidash <ashish.thapa477@gmail.com>
2024-12-24 17:10:07 +0545

    bridge-client: add deadpool manager for websocket client for restart

--- bin/bridge-client/src/modes/operator/task_manager.rs
+++ bin/bridge-client/src/modes/operator/task_manager.rs
@@ -18,21 +18,19 @@ use strata_storage::ops::{bridge_duty::BridgeDutyOps, bridge_duty_index::BridgeD
 use tokio::{task::JoinSet, time::sleep};
 use tracing::{error, info, trace, warn};
 
-pub(super) struct TaskManager<L2Client, TxBuildContext, Bcast>
+pub(super) struct TaskManager<TxBuildContext, Bcast>
 where
-    L2Client: StrataApiClient + Sync + Send,
     TxBuildContext: BuildContext + Sync + Send,
     Bcast: Broadcaster,
 {
-    pub(super) exec_handler: Arc<ExecHandler<L2Client, TxBuildContext>>,
+    pub(super) exec_handler: Arc<ExecHandler<TxBuildContext>>,
     pub(super) broadcaster: Arc<Bcast>,
     pub(super) bridge_duty_db_ops: Arc<BridgeDutyOps>,
     pub(super) bridge_duty_idx_db_ops: Arc<BridgeDutyIndexOps>,
 }
 
-impl<L2Client, TxBuildContext, Bcast> TaskManager<L2Client, TxBuildContext, Bcast>
+impl<TxBuildContext, Bcast> TaskManager<TxBuildContext, Bcast>
 where
-    L2Client: StrataApiClient + Sync + Send + 'static,
     TxBuildContext: BuildContext + Sync + Send + 'static,
     Bcast: Broadcaster + Sync + Send + 'static,
 {
@@ -85,13 +83,12 @@ where
             .unwrap_or(Some(0))
             .unwrap_or(0);
 
+        let l2_rpc_client = self.exec_handler.l2_rpc_client_pool.get().await.expect("cannot get rpc client");
         let RpcBridgeDuties {
             duties,
             start_index,
             stop_index,
-        } = self
-            .exec_handler
-            .l2_rpc_client
+        } = l2_rpc_client
             .get_bridge_duties(self.exec_handler.own_index, start_index)
             .await?;
 
@@ -150,14 +147,13 @@ where
 /// # Errors
 ///
 /// If the duty fails to be processed.
-async fn process_duty<L2Client, TxBuildContext, Bcast>(
-    exec_handler: Arc<ExecHandler<L2Client, TxBuildContext>>,
+async fn process_duty<TxBuildContext, Bcast>(
+    exec_handler: Arc<ExecHandler<TxBuildContext>>,
     duty_status_ops: Arc<BridgeDutyOps>,
     broadcaster: Arc<Bcast>,
     duty: &BridgeDuty,
 ) -> ExecResult<()>
 where
-    L2Client: StrataApiClient + Sync + Send,
     TxBuildContext: BuildContext + Sync + Send,
     Bcast: Broadcaster,
 {
@@ -212,15 +208,14 @@ where
 /// # Errors
 ///
 /// If there is an error during the execution of the duty.
-async fn execute_duty<L2Client, TxBuildContext, Tx, Bcast>(
-    exec_handler: Arc<ExecHandler<L2Client, TxBuildContext>>,
+async fn execute_duty<TxBuildContext, Tx, Bcast>(
+    exec_handler: Arc<ExecHandler<TxBuildContext>>,
     broadcaster: Arc<Bcast>,
     duty_status_ops: Arc<BridgeDutyOps>,
     tracker_txid: Txid,
     tx_info: Tx,
 ) -> ExecResult<()>
 where
-    L2Client: StrataApiClient + Sync + Send,
     TxBuildContext: BuildContext + Sync + Send,
     Tx: TxKind + Debug,
     Bcast: Broadcaster,
@@ -280,13 +275,12 @@ where
 
 /// Aggregates nonces and signatures for a given [`Txid`] and then, broadcasts the fully signed
 /// transaction to Bitcoin.
-async fn aggregate_and_broadcast<L2Client, TxBuildContext, Bcast>(
-    exec_handler: Arc<ExecHandler<L2Client, TxBuildContext>>,
+async fn aggregate_and_broadcast<TxBuildContext, Bcast>(
+    exec_handler: Arc<ExecHandler<TxBuildContext>>,
     broadcaster: Arc<Bcast>,
     txid: &Txid,
 ) -> ExecResult<()>
 where
-    L2Client: StrataApiClient + Sync + Send,
     TxBuildContext: BuildContext + Sync + Send,
     Bcast: Broadcaster,
 {
---
+++ crates/bridge-exec/src/ws_client.rs
@@ -0,0 +1,128 @@
+use core::fmt;
+
+use deadpool::managed::{self, Manager, RecycleError, RecycleResult};
+use jsonrpsee::{core::{async_trait, client::{BatchResponse, ClientT}, params::BatchRequestBuilder, traits::ToRpcParams, BoxError, DeserializeOwned},  ws_client::{WsClient as WebsocketClient, WsClientBuilder}};
+use jsonrpsee::core::ClientError;
+
+#[derive(Clone, Debug)]
+pub struct WsClientConfig {
+    pub url: String
+}
+
+#[derive(Clone, Debug)]
+pub struct WsClientManager {
+    pub config: WsClientConfig
+}
+
+#[derive(Debug)]
+pub enum WsClientState {
+    Working(WebsocketClient),
+    NotWorking
+}
+
+#[derive(Debug)]
+pub struct WsClient(WsClientState);
+
+impl Manager for WsClientManager {
+    type Type = WsClient;
+
+    type Error = jsonrpsee::core::StringError;
+
+    async fn create(&self) -> Result<Self::Type, Self::Error> {
+        let client = WsClientBuilder::default()
+            .build(self.config.url.clone())
+            .await;
+        let bl = match client {
+            Ok(cl) => WsClientState::Working(cl),
+            Err(_) => WsClientState::NotWorking,
+        };
+        Ok(WsClient(bl))
+    }
+
+    async fn recycle(
+        &self,
+        obj: &mut Self::Type,
+        _metrics: &managed::Metrics,
+    ) -> RecycleResult<Self::Error> {
+        match &obj.0 {
+            WsClientState::Working(cl) => {
+                if cl.is_connected() {
+                    Ok(())
+                } else {
+                    Err(RecycleError::Message("Connection lost, recreate client".to_string().into()))
+                }
+            },
+            WsClientState::NotWorking => {
+                  Err(RecycleError::Message("Connection still not found, recreate client".to_string().into()))
+            },
+        }
+    }
+}
+
+#[async_trait]
+impl ClientT for WsClient {
+    /// Send a [notification request](https://www.jsonrpc.org/specification#notification).
+    ///
+    /// Notifications do not produce a response on the JSON-RPC server.
+    async fn notification<Params>(
+        &self,
+        method: &str,
+        params: Params,
+    ) -> Result<(), ClientError>
+    where
+        Params: ToRpcParams + Send ,
+    {
+        match &self.0 {
+            WsClientState::Working(inner) => {
+                inner.notification(method, params).await
+            }
+            WsClientState::NotWorking => Err(ClientError::Transport(
+                BoxError::from("Client is NotWorking".to_string()),
+            )),
+        }
+    }
+
+    /// Send a [method call request](https://www.jsonrpc.org/specification#request_object).
+    ///
+    /// Returns `Ok` if the server responds successfully, otherwise a `ClientError`.
+    async fn request<R, Params>(
+        &self,
+        method: &str,
+        params: Params,
+    ) -> Result<R, ClientError>
+    where
+        R: DeserializeOwned,
+        Params: ToRpcParams + Send,
+    {
+        match &self.0 {
+            WsClientState::Working(inner) => {
+                inner.request(method, params).await
+            }
+            WsClientState::NotWorking => Err(ClientError::Transport(
+                BoxError::from("Client is NotWorking".to_string()),
+            )),
+        }
+    }
+
+    /// Send a [batch request](https://www.jsonrpc.org/specification#batch).
+    ///
+    /// The responses to the batch are returned in the same order as the requests were inserted.
+    ///
+    /// Returns `Ok` if all requests in the batch were answered, otherwise `Err(ClientError)`.
+    async fn batch_request<'a, R>(
+        &self,
+        batch: BatchRequestBuilder<'a>,
+    ) -> Result<BatchResponse<'a, R>, ClientError>
+    where
+        R: DeserializeOwned + fmt::Debug + 'a,
+    {
+        match &self.0 {
+            WsClientState::Working(inner) => {
+                inner.batch_request(batch).await
+            }
+            WsClientState::NotWorking => Err(ClientError::Transport(
+                BoxError::from("Client is NotWorking".to_string()),
+            )),
+        }
+    }
+}
