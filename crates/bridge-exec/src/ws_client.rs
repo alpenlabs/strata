6a04fffc6564c81a36e533728356a49dfbd73c99
voidash <ashish.thapa477@gmail.com>
2024-12-26 17:54:02 +0545

    bridge-exec: add comments for ws-client and formatting

--- crates/bridge-exec/src/ws_client.rs
+++ crates/bridge-exec/src/ws_client.rs
@@ -1,3 +1,6 @@
+//! Wrapper for managing a WebSocket client that allows recycling or restarting
+//! the client without restarting the entire program.
+
 use core::fmt;
 
 use deadpool::managed::{self, Manager, RecycleError, RecycleResult};
@@ -12,30 +15,58 @@ use jsonrpsee::{
     ws_client::{WsClient as WebsocketClient, WsClientBuilder},
 };
 
+/// Configuration for the WebSocket client.
+///
+/// settings that are necessary to initialize and configure
+/// the WebSocket client, ie. URL of the WebSocket server.
 #[derive(Clone, Debug)]
 pub struct WsClientConfig {
+    /// The URL of the WebSocket server.
     pub url: String,
 }
 
+/// Manager for creating and recycling WebSocket clients.
+///
+/// This struct manages the lifecycle of WebSocket clients, including creating
+/// new clients and determining whether existing clients are still valid.
 #[derive(Clone, Debug)]
 pub struct WsClientManager {
+    /// The configuration used to initialize WebSocket clients.
     pub config: WsClientConfig,
 }
 
+/// Represents the state of a WebSocket client.
+///
+/// - `Working`: The client is connected and operational.
+/// - `NotWorking`: The client is not connected or has encountered an error.
 #[derive(Debug)]
 pub enum WsClientState {
+    /// The WebSocket client is connected and operational.
     Working(WebsocketClient),
+    /// The WebSocket client is not connected or is in a failed state.
     NotWorking,
 }
 
+/// Wrapper for the WebSocket client state.
+///
+/// This struct encapsulates the `WsClientState`, enabling unified management of
+/// both connected and failed client states.
 #[derive(Debug)]
 pub struct WsClient(WsClientState);
 
+/// Implements the `Manager` trait for managing WebSocket clients.
+///
+/// The `WsClientManager` provides methods to create new clients and recycle
+/// existing ones, ensuring that clients remain in a valid state.
 impl Manager for WsClientManager {
     type Type = WsClient;
-
     type Error = jsonrpsee::core::StringError;
 
+    /// Creates a new WebSocket client.
+    ///
+    /// Attempts to initialize a new WebSocket client using the provided configuration.
+    /// Returns a `WsClient` wrapped in a `Working` or `NotWorking` state depending on
+    /// the success of the operation.
     async fn create(&self) -> Result<Self::Type, Self::Error> {
         let client = WsClientBuilder::default()
             .build(self.config.url.clone())
@@ -47,6 +78,11 @@ impl Manager for WsClientManager {
         Ok(WsClient(bl))
     }
 
+    /// Recycles an existing WebSocket client.
+    ///
+    /// Checks whether the provided client is still valid. If the client is connected,
+    /// it is retained. Otherwise, an error is returned, indicating the need to recreate
+    /// the client.
     async fn recycle(
         &self,
         obj: &mut Self::Type,
@@ -71,6 +107,10 @@ impl Manager for WsClientManager {
     }
 }
 
+/// Implements the `ClientT` trait for `WsClient`.
+///
+/// This implementation allows `WsClient` to perform JSON-RPC operations,
+/// including notifications, method calls, and batch requests.
 #[async_trait]
 impl ClientT for WsClient {
     /// Send a [notification request](https://www.jsonrpc.org/specification#notification).
@@ -104,11 +144,7 @@ impl ClientT for WsClient {
         }
     }
 
-    /// Send a [batch request](https://www.jsonrpc.org/specification#batch).
-    ///
-    /// The responses to the batch are returned in the same order as the requests were inserted.
-    ///
-    /// Returns `Ok` if all requests in the batch were answered, otherwise `Err(ClientError)`.
+    /// Sends a batch request.
     async fn batch_request<'a, R>(
         &self,
         batch: BatchRequestBuilder<'a>,
@@ -119,7 +155,7 @@ impl ClientT for WsClient {
         match &self.0 {
             WsClientState::Working(inner) => inner.batch_request(batch).await,
             WsClientState::NotWorking => Err(ClientError::Transport(BoxError::from(
-                "Client is NotWorking".to_string(),
+                "Client is Not Working".to_string(),
             ))),
         }
     }
