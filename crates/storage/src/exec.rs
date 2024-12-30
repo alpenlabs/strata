85b42fb7f3d7e802e2cf0e4ba1db4e552e711b0b
Bibek Pandey <bibek@alpenlabs.com>
2024-12-30 14:59:14 +0545

    (storage): Add inst_ops_auto that automatically creates the functions

--- crates/storage/src/exec.rs
+++ crates/storage/src/exec.rs
@@ -135,4 +135,83 @@ macro_rules! inst_ops {
     }
 }
 
+/// Same as `inst_ops` but automatically generates the functions. The function name must exist
+/// inside the database instance
+macro_rules! inst_ops_auto {
+    {
+        ($db_method: ident, $base:ident, $ctx:ident $(<$($tparam:ident: $tpconstr:tt),+>)?) {
+            $($iname:ident($($aname:ident: $aty:ty),*) => $ret:ty;)*
+        }
+    } => {
+        pub struct $base {
+            pool: threadpool::ThreadPool,
+            inner: Arc<dyn ShimTrait>,
+        }
+
+        paste::paste! {
+            impl $base {
+                pub fn new $(<$($tparam: $tpconstr + Sync + Send + 'static),+>)? (pool: threadpool::ThreadPool, ctx: Arc<$ctx $(<$($tparam),+>)?>) -> Self {
+                    Self {
+                        pool,
+                        inner: Arc::new(Inner { ctx }),
+                    }
+                }
+
+                $(
+                    pub async fn [<$iname _async>] (&self, $($aname: $aty),*) -> DbResult<$ret> {
+                        let resp_rx = self.inner. [<$iname _chan>] (&self.pool, $($aname),*);
+                        match resp_rx.await {
+                            Ok(v) => v,
+                            Err(_e) => Err(DbError::WorkerFailedStrangely),
+                        }
+                    }
+
+                    pub fn [<$iname _blocking>] (&self, $($aname: $aty),*) -> DbResult<$ret> {
+                        self.inner. [<$iname _blocking>] ($($aname),*)
+                    }
+
+                    pub fn [<$iname _chan>] (&self, $($aname: $aty),*) -> DbRecv<$ret> {
+                        self.inner. [<$iname _chan>] (&self.pool, $($aname),*)
+                    }
+                )*
+            }
+
+            #[async_trait::async_trait]
+            trait ShimTrait: Sync + Send + 'static {
+                $(
+                    fn [<$iname _blocking>] (&self, $($aname: $aty),*) -> DbResult<$ret>;
+                    fn [<$iname _chan>] (&self, pool: &threadpool::ThreadPool, $($aname: $aty),*) -> DbRecv<$ret>;
+                )*
+            }
+
+            pub struct Inner $(<$($tparam: $tpconstr + Sync + Send + 'static),+>)? {
+                ctx: Arc<$ctx $(<$($tparam),+>)?>,
+            }
+
+            impl $(<$($tparam: $tpconstr + Sync + Send + 'static),+>)? ShimTrait for Inner $(<$($tparam),+>)? {
+                $(
+                    fn [<$iname _blocking>] (&self, $($aname: $aty),*) -> DbResult<$ret> {
+                        self.ctx.db.$db_method().$iname($($aname),*)
+                    }
+
+                    fn [<$iname _chan>] (&self, pool: &threadpool::ThreadPool, $($aname: $aty),*) -> DbRecv<$ret> {
+                        let (resp_tx, resp_rx) = tokio::sync::oneshot::channel();
+                        let ctx = self.ctx.clone();
+
+                        pool.execute(move || {
+                            let res = ctx.db.$db_method().$iname($($aname),*);
+                            if resp_tx.send(res).is_err() {
+                                warn!("failed to send response");
+                            }
+                        });
+
+                        resp_rx
+                    }
+                )*
+            }
+        }
+    }
+}
+
 pub(crate) use inst_ops;
+pub(crate) use inst_ops_auto;
