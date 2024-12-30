f42e789c07ad4cb18b0e017121cc61b26fc94802
Bibek Pandey <bibek@alpenlabs.com>
2024-12-30 15:12:55 +0545

    (storage): Use inst_ops_auto for broadcast db

--- crates/storage/src/ops/l1tx_broadcast.rs
+++ crates/storage/src/ops/l1tx_broadcast.rs
@@ -1,10 +1,6 @@
 use std::sync::Arc;
 
-use strata_db::{
-    traits::*,
-    types::{L1TxEntry, L1TxStatus},
-    DbResult,
-};
+use strata_db::{traits::*, types::L1TxEntry, DbResult};
 use strata_primitives::buf::Buf32;
 
 use crate::exec::*;
@@ -24,73 +20,13 @@ impl<D: BroadcastDatabase + Sync + Send + 'static> Context<D> {
     }
 }
 
-inst_ops! {
-    (BroadcastDbOps, Context<D: BroadcastDatabase>) {
+inst_ops_auto! {
+    (l1_broadcast_db, BroadcastDbOps, Context<D: BroadcastDatabase>) {
         get_tx_entry(idx: u64) => Option<L1TxEntry>;
         get_tx_entry_by_id(id: Buf32) => Option<L1TxEntry>;
-        get_tx_status(id: Buf32) => Option<L1TxStatus>;
         get_txid(idx: u64) => Option<Buf32>;
         get_next_tx_idx() => u64;
         put_tx_entry(id: Buf32, entry: L1TxEntry) => Option<u64>;
         put_tx_entry_by_idx(idx: u64, entry: L1TxEntry) => ();
     }
-}
-
-fn get_tx_entry<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-    idx: u64,
-) -> DbResult<Option<L1TxEntry>> {
-    let bcast_db = context.db.l1_broadcast_db();
-    bcast_db.get_tx_entry(idx)
-}
-
-fn get_tx_entry_by_id<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-    id: Buf32,
-) -> DbResult<Option<L1TxEntry>> {
-    let bcast_db = context.db.l1_broadcast_db();
-    bcast_db.get_tx_entry_by_id(id)
-}
-
-fn get_txid<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-    idx: u64,
-) -> DbResult<Option<Buf32>> {
-    let bcast_db = context.db.l1_broadcast_db();
-    bcast_db.get_txid(idx)
-}
-
-fn get_tx_status<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-    id: Buf32,
-) -> DbResult<Option<L1TxStatus>> {
-    let bcast_db = context.db.l1_broadcast_db();
-    Ok(bcast_db.get_tx_entry_by_id(id)?.map(|entry| entry.status))
-}
-
-fn get_next_tx_idx<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-) -> DbResult<u64> {
-    let bcast_db = context.db.l1_broadcast_db();
-    bcast_db.get_next_tx_idx()
-}
-
-fn put_tx_entry<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-    txid: Buf32,
-    entry: L1TxEntry,
-) -> DbResult<Option<u64>> {
-    trace!(%txid, "insert_new_tx_entry");
-    assert!(entry.try_to_tx().is_ok(), "invalid tx entry {entry:?}");
-    let bcast_db = context.db.l1_broadcast_db();
-    bcast_db.put_tx_entry(txid, entry)
-}
-
-fn put_tx_entry_by_idx<D: BroadcastDatabase + Sync + Send + 'static>(
-    context: &Context<D>,
-    idx: u64,
-    entry: L1TxEntry,
-) -> DbResult<()> {
-    let bcast_db = context.db.l1_broadcast_db();
-    bcast_db.put_tx_entry_by_idx(idx, entry)
 }
