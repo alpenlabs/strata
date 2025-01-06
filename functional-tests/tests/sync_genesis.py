ca171691e383546eb0e7e33e9e365784bc97918a
Trey Del Bonis <trey@alpenlabs.io>
2025-01-06 16:03:05 -0500

    fntest: fixed sync_genesis, converted some logs to prints (not sure why they weren't printing)

--- functional-tests/tests/sync_genesis.py
+++ functional-tests/tests/sync_genesis.py
@@ -11,7 +11,7 @@ MAX_GENESIS_TRIES = 10
 @flexitest.register
 class SyncGenesisTest(testenv.StrataTester):
     def __init__(self, ctx: flexitest.InitContext):
-        ctx.set_env("basic")
+        ctx.set_env(testenv.BasicEnvConfig(100))
 
     def main(self, ctx: flexitest.RunContext):
         seq = ctx.get_service("sequencer")
@@ -19,7 +19,7 @@ class SyncGenesisTest(testenv.StrataTester):
         # create both btc and sequencer RPC
         seqrpc = seq.create_rpc()
 
-        time.sleep(3)
+        time.sleep(5)
 
         # Wait until genesis.  This might need to be tweaked if we change how
         # long we wait for genesis in tests.
@@ -28,16 +28,16 @@ class SyncGenesisTest(testenv.StrataTester):
         while True:
             assert tries <= MAX_GENESIS_TRIES, "did not observe genesis before timeout"
 
-            self.debug("waiting for genesis")
+            print("waiting for genesis")
             stat = seqrpc.strata_clientStatus()
             self.debug(stat)
             if stat["finalized_blkid"] != UNSET_ID:
                 last_slot = stat["chain_tip_slot"]
-                self.debug(f"observed genesis, now at slot {last_slot}")
+                print(f"observed genesis, now at slot {last_slot}")
                 break
 
             time.sleep(0.5)
-            self.debug(f"waiting for genesis... -- tries {tries}")
+            print(f"waiting for genesis... -- tries {tries}")
             tries += 1
 
         assert last_slot is not None, "last slot never set"
@@ -49,7 +49,7 @@ class SyncGenesisTest(testenv.StrataTester):
             stat = seqrpc.strata_clientStatus()
             tip_slot = stat["chain_tip_slot"]
             tip_blkid = stat["chain_tip"]
-            self.debug(f"cur tip slot {tip_slot} blkid { tip_blkid}")
+            print(f"cur tip slot {tip_slot} blkid { tip_blkid}")
             assert tip_slot >= last_slot, "cur slot went backwards"
             assert tip_slot > last_slot, "seem to not be making progress"
             last_slot = tip_slot
