086b19d1c0965e7b70e867d844d487fa3372d888
voidash <ashish.thapa477@gmail.com>
2025-01-06 08:41:04 +0545

    func-test: increase timeout for deposit confirmation

--- functional-tests/fn_bridge_deposit_seq_unreliable.py
+++ functional-tests/fn_bridge_deposit_seq_unreliable.py
@@ -53,7 +53,7 @@ class BridgeDepositSequencerUnreliableTest(testenv.BridgeTestBase):
         balance_after_deposits = wait_until_with_value(
             lambda: int(self.rethrpc.eth_getBalance(el_address), 16),
             predicate=lambda v: v == 2 * cfg.deposit_amount * SATS_TO_WEI,
-            timeout=15,
+            timeout=30,
         )
         self.debug(f"Strata Balance after deposits: {balance_after_deposits}")
 
