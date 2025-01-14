d34421c985e30e87c5080299591283082852d6c9
Abishek Bashyal <abishek@alpenlabs.io>
2025-01-14 19:42:36 +0545

    Prover Test: Add Configurable Proof File Output (#609)
    enable GENERATE_PROOF_FILE file

--- crates/zkvm/adapters/sp1/src/host.rs
+++ crates/zkvm/adapters/sp1/src/host.rs
@@ -212,9 +212,11 @@ mod tests {
             SP1ProofReceipt::try_from(proof).expect("Failed to convert to SP1ProofReceipt");
         let proof_data = sp1_proof.inner();
 
-        let filename = "proof-groth16.bin";
-        let mut file = File::create(filename).unwrap();
-        file.write_all(&bincode::serialize(&proof_data).expect("bincode serialization failed"))
-            .unwrap();
+        if std::env::var("GENERATE_PROOF_FILE").is_ok() {
+            let filename = "proof-groth16.bin";
+            let mut file = File::create(filename).expect("Failed to create proof-groth16.bin");
+            file.write_all(&bincode::serialize(&proof_data).expect("bincode serialization fail"))
+                .expect("Failed to write to proof-groth16.bin");
+        }
     }
 }
