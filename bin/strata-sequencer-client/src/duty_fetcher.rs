59d6c800ac9116006e8e42f79cc3a6ac29af3b21
Sapin Bajracharya <sapin@alpenlabs.io>
2025-01-17 16:52:53 +0545

    refactor: move duty fetcher worker to own file

---
+++ bin/strata-sequencer-client/src/duty_fetcher.rs
@@ -0,0 +1,37 @@
+use std::{sync::Arc, time::Duration};
+
+use strata_rpc_api::StrataSequencerApiClient;
+use strata_sequencer::duty::types::Duty;
+use tokio::sync::mpsc;
+use tracing::{error, warn};
+
+pub(crate) async fn duty_fetcher_worker<R>(
+    rpc: Arc<R>,
+    duty_tx: mpsc::Sender<Duty>,
+    poll_interval: u64,
+) -> anyhow::Result<()>
+where
+    R: StrataSequencerApiClient + Send + Sync + 'static,
+{
+    let mut interval = tokio::time::interval(Duration::from_millis(poll_interval));
+    'top: loop {
+        interval.tick().await;
+        let duties = match rpc.get_sequencer_duties().await {
+            Ok(duties) => duties,
+            Err(err) => {
+                // log error and try again
+                error!("duty_fetcher_worker: failed to get duties: {}", err);
+                continue;
+            }
+        };
+
+        for duty in duties {
+            if duty_tx.send(duty).await.is_err() {
+                warn!("duty_fetcher_worker: rx dropped; exiting");
+                break 'top;
+            }
+        }
+    }
+
+    Ok(())
+}
