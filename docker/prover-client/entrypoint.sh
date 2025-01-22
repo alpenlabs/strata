4647c8a24080f7e5c84af0d3cf803edd0308ce84
abishek <abishek@alpenlabs.io>
2025-01-23 01:52:13 +0545

    prover doc img docs added

--- docker/prover-client/entrypoint.sh
+++ docker/prover-client/entrypoint.sh
@@ -7,7 +7,10 @@ echo "starting Prover client"
 
 strata-prover-client \
     --rpc-port 9851 \
-    --sequencer-rpc http://sequencer:8432 \
     --enable-dev-rpcs true \
     --enable-checkpoint-runner false \
+    --sequencer-rpc http://sequencer:8432 \
+    --bitcoind-url http://bitcoind:8332 \
+    --bitcoind-user $BITCOIND_USER \
+    --bitcoind-password", $BITCOIND_PASSWORD \
     --reth-rpc http://reth:8545 $@
