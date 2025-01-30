9ea9a57063efb7614e707147275fc59a36f8dd49
Trey Del Bonis <trey@alpenlabs.io>
2025-01-30 14:02:18 -0500

    storage: fixed broken identifiers after rebase

--- crates/storage/src/ops/chainstate.rs
+++ crates/storage/src/ops/chainstate.rs
@@ -3,7 +3,7 @@
 use std::sync::Arc;
 
 use strata_db::traits::*;
-use strata_state::{chain_state::ChainState, state_op::WriteBatch};
+use strata_state::{chain_state::Chainstate, state_op::WriteBatch};
 
 use crate::exec::*;
 
@@ -23,18 +23,18 @@ impl<D: Database + Sync + Send + 'static> Context<D> {
 
 inst_ops! {
     (ChainstateDataOps, Context<D: Database>) {
-        write_genesis_state(toplevel: ChainState) => ();
+        write_genesis_state(toplevel: Chainstate) => ();
         write_state_update(idx: u64, batch: WriteBatch) => ();
         get_last_state_idx() => u64;
         get_earliest_state_idx() => u64;
         get_writes_at(idx: u64) => Option<WriteBatch>;
-        get_toplevel_state(idx: u64) => Option<ChainState>;
+        get_toplevel_state(idx: u64) => Option<Chainstate>;
         // TODO the rest, not including yet because might iterate
     }
 }
 
-fn write_genesis_state<D: Database>(context: &Context<D>, toplevel: ChainState) -> DbResult<()> {
-    let chs = context.db.chain_state_store();
+fn write_genesis_state<D: Database>(context: &Context<D>, toplevel: Chainstate) -> DbResult<()> {
+    let chs = context.db.chain_state_db();
     chs.write_genesis_state(&toplevel)?;
     Ok(())
 }
@@ -44,23 +44,23 @@ fn write_state_update<D: Database>(
     idx: u64,
     batch: WriteBatch,
 ) -> DbResult<()> {
-    let chs = context.db.chain_state_store();
+    let chs = context.db.chain_state_db();
     chs.write_state_update(idx, &batch)?;
     Ok(())
 }
 
 fn get_last_state_idx<D: Database>(context: &Context<D>) -> DbResult<u64> {
-    context.db.chain_state_provider().get_last_state_idx()
+    context.db.chain_state_db().get_last_state_idx()
 }
 
 fn get_earliest_state_idx<D: Database>(context: &Context<D>) -> DbResult<u64> {
-    context.db.chain_state_provider().get_earliest_state_idx()
+    context.db.chain_state_db().get_earliest_state_idx()
 }
 
 fn get_writes_at<D: Database>(context: &Context<D>, idx: u64) -> DbResult<Option<WriteBatch>> {
-    context.db.chain_state_provider().get_writes_at(idx)
+    context.db.chain_state_db().get_writes_at(idx)
 }
 
-fn get_toplevel_state<D: Database>(context: &Context<D>, idx: u64) -> DbResult<Option<ChainState>> {
-    context.db.chain_state_provider().get_toplevel_state(idx)
+fn get_toplevel_state<D: Database>(context: &Context<D>, idx: u64) -> DbResult<Option<Chainstate>> {
+    context.db.chain_state_db().get_toplevel_state(idx)
 }
