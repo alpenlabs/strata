901da00167fed045b2dcd4b57db111cda2139766
abishek <abishek@alpenlabs.io>
2025-02-04 00:41:09 +0545

    refactor schnoor test

--- functional-tests/tests/el_schnorr_precompile.py
+++ functional-tests/tests/el_schnorr_precompile.py
@@ -1,12 +1,12 @@
-import hashlib
-
 import flexitest
-from strata_utils import sign_schnorr_sig
 from web3 import Web3
 
 from envs import testenv
-from utils import wait_until_with_value
-from utils.constants import PRECOMPILE_SCHNORR_ADDRESS
+from utils.schnorr import (
+    get_precompile_input,
+    make_schnorr_precompile_call,
+    get_test_schnnor_secret_key,
+)
 
 
 @flexitest.register
@@ -25,62 +25,21 @@ class SchnorrPrecompileTest(testenv.StrataTester):
         This test checks for the valid and invalid input for this precompile
         """
         reth = ctx.get_service("reth")
-        self.web3: Web3 = reth.create_web3()
-
-        self.source = self.web3.address
-        self.dest = self.web3.to_checksum_address(PRECOMPILE_SCHNORR_ADDRESS)
+        web3: Web3 = reth.create_web3()
 
         # secret key
-        secret_key = "a9f913c3d7fe56c462228ad22bb7631742a121a6a138d57c1fc4a351314948fa"
-        self.debug(secret_key)
-
-        message_hash = hashlib.sha256(b"AlpenStrata").hexdigest()
-        (signature, public_key) = sign_schnorr_sig(message_hash, secret_key)
-        signature = signature.hex()
-        public_key = public_key.hex()
-
-        valid_precompile_input = public_key + message_hash + signature
-        data = self.wait_for_precompile_response(valid_precompile_input)
+        secret_key = get_test_schnnor_secret_key()
+        msg = "AlpenStrata"
+        precompile_input = get_precompile_input(secret_key, msg)
+        _txid, data = make_schnorr_precompile_call(web3, precompile_input)
         assert data == "0x01", f"Schnorr verification failed: expected '0x01', got '{data}'."
 
-        invalid_message = hashlib.sha256(b"MakaluStrata").hexdigest()
-        invalid_precompile_input = public_key + invalid_message + signature
-        data = self.wait_for_precompile_response(invalid_precompile_input)
+        another_message = "MakaluStrata"
+        another_precompile_input = get_precompile_input(secret_key, another_message)
+
+        # Precompile input: Public Key (64) || Message Hash (64) || Signature (128)
+        modified_precompile_input = another_precompile_input[:-128] + precompile_input[-128:]
+        _txid, data = make_schnorr_precompile_call(web3, modified_precompile_input)
         assert data == "0x00", f"Schnorr verification failed: expected '0x00', got '{data}'."
 
         return True
-
-    def wait_for_precompile_response(self, precompile_input: str):
-        assert self.web3.is_connected(), "cannot connect to reth"
-        txid = self.schnorr_precompile(precompile_input)
-
-        receipt = wait_until_with_value(
-            lambda: self.web3.eth.get_transaction_receipt(txid),
-            lambda x: not isinstance(x, Exception),
-            error_with="Transaction receipt for txid not available",
-        )
-
-        assert receipt.status == 1, "precompile transaction failed"
-
-        data = self.web3.eth.call(
-            {
-                "to": self.dest,
-                "data": precompile_input,
-            }
-        )
-        return data.to_0x_hex()
-
-    def schnorr_precompile(self, data: str):
-        # No Wei needs to be sent for this precompile
-        to_transfer_wei = 0
-
-        txid = self.web3.eth.send_transaction(
-            {
-                "to": self.dest,
-                "value": hex(to_transfer_wei),
-                "gas": hex(100000),
-                "from": self.source,
-                "data": data,
-            }
-        )
-        return txid
---
+++ functional-tests/utils/schnorr.py
@@ -0,0 +1,79 @@
+import hashlib
+from strata_utils import sign_schnorr_sig
+from web3 import Web3
+from utils import wait_until_with_value
+from utils.constants import PRECOMPILE_SCHNORR_ADDRESS
+from typing import Tuple
+
+
+def get_precompile_input(secret_key: str, msg: str) -> str:
+    """
+    Generates the strata schnorr precompile input by signing the SHA-256 hash of the message.
+
+    Args:
+        secret_key (str): The secret key used for signing.
+        msg (str): The message to sign.
+
+    Returns:
+        str: Schnorr precompile input i.e concatenated hex string of the public key, message hash, and signature.
+    """
+    msg_bytes = msg.encode("utf-8")
+    message_hash = hashlib.sha256(msg_bytes).hexdigest()
+    signature, public_key = sign_schnorr_sig(message_hash, secret_key)
+
+    return public_key.hex() + message_hash + signature.hex()
+
+
+def make_schnorr_precompile_call(web3: Web3, precompile_input: str) -> Tuple[str, str]:
+    """
+    Executes a Schnorr precompile call.
+
+    Args:
+        web3 (Web3): An instance of Web3.
+        precompile_input (str): The input data for the precompile.
+
+    Returns:
+        Tuple[str, str]: A tuple containing the transaction hash and result of precompile call.
+
+    Raises:
+        ConnectionError: If unable to connect to the blockchain.
+        RuntimeError: If the transaction fails.
+    """
+    if not web3.is_connected():
+        raise ConnectionError("Cannot connect to reth")
+
+    source = web3.address
+    destination = web3.to_checksum_address(PRECOMPILE_SCHNORR_ADDRESS)
+
+    # Simulate the precompile call (safe because precompile is stateless)
+    simulated_result = web3.eth.call(
+        {
+            "to": destination,
+            "data": precompile_input,
+        }
+    )
+
+    tx_params = {
+        "to": destination,
+        "from": source,
+        "value": hex(0),
+        "gas": hex(100000),
+        "data": precompile_input,
+    }
+    txid = web3.eth.send_transaction(tx_params)
+
+    receipt = wait_until_with_value(
+        lambda: web3.eth.get_transaction_receipt(txid),
+        lambda result: not isinstance(result, Exception),
+        error_with="Transaction receipt for txid not available",
+    )
+
+    if receipt.status != 1:
+        raise RuntimeError("Precompile transaction failed")
+
+    return txid, simulated_result.to_0x_hex()
+
+
+def get_test_schnnor_secret_key() -> str:
+    """Return the test Schnnor secret key."""
+    return "a9f913c3d7fe56c462228ad22bb7631742a121a6a138d57c1fc4a351314948fa"
