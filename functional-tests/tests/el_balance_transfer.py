2554ba8e47461b25645e035103484f48226ced2d
Abishek Bashyal <abishek@alpenlabs.io>
2025-02-05 11:42:21 +0545

    Prover EL: Test non empty block (#651)
    * prover el test non empty block
    
    * lint fix

--- functional-tests/tests/el_balance_transfer.py
+++ functional-tests/tests/el_balance_transfer.py
@@ -1,9 +1,15 @@
-import time
-
 import flexitest
 from web3 import Web3
 
 from envs import testenv
+from utils.eth import make_native_token_transfer
+
+NATIVE_TOKEN_TRANSFER_PARAMS = {
+    "DEST_ADDRESS": "0x0000000000000000000000000000000000000001",
+    "BASEFEE_ADDRESS": "5400000000000000000000000000000000000010",
+    "BENEFICIARY_ADDRESS": "5400000000000000000000000000000000000011",
+    "TRANSFER_AMOUNT": Web3.to_wei(1, "ether"),
+}
 
 
 @flexitest.register
@@ -13,13 +19,14 @@ class ElBalanceTransferTest(testenv.StrataTester):
 
     def main(self, ctx: flexitest.RunContext):
         reth = ctx.get_service("reth")
-
         web3: Web3 = reth.create_web3()
 
         source = web3.address
-        dest = web3.to_checksum_address("0x0000000000000000000000000000000000000001")
-        basefee_address = web3.to_checksum_address("5400000000000000000000000000000000000010")
-        beneficiary_address = web3.to_checksum_address("5400000000000000000000000000000000000011")
+        dest = web3.to_checksum_address(NATIVE_TOKEN_TRANSFER_PARAMS["DEST_ADDRESS"])
+        basefee_address = web3.to_checksum_address(NATIVE_TOKEN_TRANSFER_PARAMS["BASEFEE_ADDRESS"])
+        beneficiary_address = web3.to_checksum_address(
+            NATIVE_TOKEN_TRANSFER_PARAMS["BENEFICIARY_ADDRESS"]
+        )
 
         self.debug(f"{web3.is_connected()}")
         original_block_no = web3.eth.block_number
@@ -30,13 +37,8 @@ class ElBalanceTransferTest(testenv.StrataTester):
 
         self.debug(f"{original_block_no}, {dest_original_balance}")
 
-        to_transfer = 1_000_000_000_000_000_000
-
-        web3.eth.send_transaction(
-            {"to": dest, "value": hex(to_transfer), "gas": hex(100000), "from": source}
-        )
-
-        time.sleep(2)
+        transfer_amount = NATIVE_TOKEN_TRANSFER_PARAMS["TRANSFER_AMOUNT"]
+        _tx_receipt = make_native_token_transfer(web3, transfer_amount, dest)
 
         final_block_no = web3.eth.block_number
         dest_final_balance = web3.eth.get_balance(dest)
@@ -47,7 +49,7 @@ class ElBalanceTransferTest(testenv.StrataTester):
         self.debug(f"{final_block_no}, {dest_final_balance}")
 
         assert original_block_no < final_block_no
-        assert dest_original_balance + to_transfer == dest_final_balance
+        assert dest_original_balance + transfer_amount == dest_final_balance
 
         basefee_balance_change = basefee_final_balance - basefee_original_balance
         assert basefee_balance_change > 0
@@ -58,6 +60,6 @@ class ElBalanceTransferTest(testenv.StrataTester):
             source_balance_change
             + basefee_balance_change
             + beneficiary_balance_change
-            + to_transfer
+            + transfer_amount
             == 0
         ), "total balance change is not balanced"
