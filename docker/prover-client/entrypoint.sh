0d0098d28439982090e9785fa1314b37706ed27f
abishek <abishek@alpenlabs.io>
2025-02-06 19:06:32 +0545

    prover client docker compose service

--- docker/prover-client/entrypoint.sh
+++ docker/prover-client/entrypoint.sh
@@ -9,11 +9,4 @@ echo "starting Prover client"
 strata-prover-client \
     --rpc-port 9851 \
     --enable-dev-rpcs true \
-    --enable-checkpoint-runner false \
-    --rollup-params $ROLLUP_PARAMS \
-    --datadir $DATA_DIR \
-    --sequencer-rpc http://sequencer:8432 \
-    --bitcoind-url http://bitcoind:8332 \
-    --bitcoind-user $BITCOIND_USER \
-    --bitcoind-password $BITCOIND_PASSWORD \
-    --reth-rpc http://reth:8545 $@
+    --enable-checkpoint-runner false $@
