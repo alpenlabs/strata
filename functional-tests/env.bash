5f36b7d1bbd2124383138c71d25d03150f8a1369
Trey Del Bonis <trey@alpenlabs.io>
2025-02-14 12:22:41 -0500

    testfw: adjusted log levels

--- functional-tests/env.bash
+++ functional-tests/env.bash
@@ -1,4 +1,4 @@
-export RUST_LOG=trace,hyper=warn,soketto=warn,jsonrpsee-server=warn,mio=warn
+export RUST_LOG=trace,hyper=warn,soketto=warn,jsonrpsee-server=warn,mio=warn,strata_btcio::rpc::client=warn
 export NO_COLOR=1
 export PATH=$PATH:$(realpath ../target/release)
 export RUST_BACKTRACE=1
