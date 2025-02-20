af8547757014fc6c3594b3978193c9dfa5b05b15
Trey Del Bonis <trey@alpenlabs.io>
2025-02-19 21:40:51 -0500

    fntest/crash_sync_event_finalize_epoch: fixed timeouts

--- functional-tests/tests/crash/crash_sync_event_finalize_epoch.py
+++ functional-tests/tests/crash/crash_sync_event_finalize_epoch.py
@@ -11,12 +11,11 @@ class CrashSyncEventFinalizeEpochTest(seq_crash_mixin.SeqCrashMixin):
         ctx.set_env(testenv.BasicEnvConfig(101))
 
     def main(self, ctx: flexitest.RunContext):
-        cur_chain_tip = self.handle_bail(lambda: "sync_event_finalize_epoch")
+        cur_chain_tip = self.handle_bail(lambda: "sync_event_finalize_epoch", timeout=60)
 
         wait_until(
-            lambda: self.seqrpc.strata_clientStatus()["chain_tip_slot"] > cur_chain_tip + 1,
-            error_with="chain tip slot not progressing",
-            timeout=60,
+            lambda: self.seqrpc.strata_syncStatus()["tip_height"] > cur_chain_tip + 1,
+            error_with="chain tip slot not progressing"
         )
 
         return True
