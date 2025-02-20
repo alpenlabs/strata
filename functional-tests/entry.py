d121b8ed596fd8aeacbe02d46e1db7ffa421a74f
Sapin Bajracharya <sapin@alpenlabs.io>
2025-02-20 12:06:53 +0545

    fntest: skip prover_el_deposit_withdraw test run prover tests in strict mode

--- functional-tests/entry.py
+++ functional-tests/entry.py
@@ -121,7 +121,7 @@ def main(argv):
         "hub1": testenv.HubNetworkEnvConfig(
             2
         ),  # TODO: Need to generate at least horizon blocks, based on params
-        "prover": testenv.BasicEnvConfig(101),
+        "prover": testenv.BasicEnvConfig(101, rollup_settings=RollupParamsSettings.new_default().strict_mode()),
         "load_reth": reth_load_env,
         # seperate env for running crash_* tests
         "crash": testenv.BasicEnvConfig(101),
