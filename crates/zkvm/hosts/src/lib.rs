b62a85e34fb0de37a2936c153602f799593dda24
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-03-05 12:04:39 +0545

    fix: update zkvm hosts

--- crates/zkvm/hosts/src/lib.rs
+++ crates/zkvm/hosts/src/lib.rs
@@ -15,7 +15,7 @@ cfg_if! {
 cfg_if! {
     if #[cfg(feature = "risc0")] {
         pub mod risc0;
-        use zkaleido_risc0_adapter::Risc0Host;
+        use zkaleido_risc0_host::Risc0Host;
 
         pub fn get_risc0_host(vm: ProofVm) -> &'static Risc0Host {
             risc0::get_host(vm)
@@ -26,7 +26,7 @@ cfg_if! {
 cfg_if! {
     if #[cfg(feature = "sp1")] {
         pub mod sp1;
-        use zkaleido_sp1_adapter::SP1Host;
+        use zkaleido_sp1_host::SP1Host;
 
         pub fn get_sp1_host(vm: ProofVm) -> &'static SP1Host {
             sp1::get_host(vm)
