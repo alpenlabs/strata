75ec073a0757fee92fc96309aa67abd8a113c10d
Sapin Bajracharya <sapin@alpenlabs.io>
2025-03-11 19:00:56 +0545

    fntest: mode numbers to constants and add comments

--- functional-tests/tests/el_batch_gas_limit.py
+++ functional-tests/tests/el_batch_gas_limit.py
@@ -7,16 +7,27 @@ from web3 import Web3
 
 from envs import testenv
 from utils.reth import get_chainconfig
+from utils.utils import RollupParamsSettings, wait_until
 
+BLOCK_GAS_LIMIT = 1_000_000
 EPOCH_GAS_LIMIT = 2_000_000
+GAS_PER_TX = 450_000
+TX_COUNT = 10
+
 
 chain_config = get_chainconfig()
-chain_config["gasLimit"] = hex(1_000_000)
+chain_config["gasLimit"] = hex(BLOCK_GAS_LIMIT)
+
+
+def block_number_available(web3, block_no):
+    return lambda: web3.eth.get_block_number() >= block_no
 
 
 @flexitest.register
-class ElBalanceTransferTest(testenv.StrataTester):
+class ElBatchGasLimitTest(testenv.StrataTester):
     def __init__(self, ctx: flexitest.InitContext):
+        rollup_settings = RollupParamsSettings.new_default()
+        rollup_settings.proof_timeout = 1000  # slow batch so we dont cross epoch while testing
         ctx.set_env(
             testenv.BasicEnvConfig(110, epoch_gas_limit=EPOCH_GAS_LIMIT, custom_chain=chain_config)
         )
@@ -24,30 +35,35 @@ class ElBalanceTransferTest(testenv.StrataTester):
     def main(self, ctx: flexitest.RunContext):
         seq_signer = ctx.get_service("sequencer_signer")
         seq_signer.stop()
-        # FIXME: process is NOT terminated yet so need to wait
+        # FIXME: process is NOT terminated immediately so need to wait
         time.sleep(1)
 
-        # seq = ctx.get_service("sequencer")
-        # seqrpc = seq.create_rpc()
         reth = ctx.get_service("reth")
         web3: Web3 = reth.create_web3()
 
         source = web3.address
         nonce = web3.eth.get_transaction_count(source)
+        # send 10 txns with GAS_PER_TX gas each
         _txids = [
-            make_gas_burner_transaction(web3, source, nonce + i, 450_000) for i in range(0, 10)
+            make_gas_burner_transaction(web3, source, nonce + i, GAS_PER_TX)
+            for i in range(0, TX_COUNT)
         ]
+        # if all txns are included, epoch gas limit should be crossed
+        assert GAS_PER_TX * TX_COUNT > EPOCH_GAS_LIMIT
 
         original_block_no = web3.eth.get_block_number()
+
+        # re-start block production
         seq_signer.start()
 
+        # we expect txns to be included in immediate next blocks
+        # wait for txns to be included in new blocks until we get consecutive empty blocks
+        # signifying either all txns are processed or epoch limit reached
         total_gas_used = 0
         block_no = original_block_no + 1
         zero_gas_blocks = 0
         while zero_gas_blocks < 2:
-            while web3.eth.get_block_number() < block_no:
-                self.info("no block yet")
-                time.sleep(1)
+            wait_until(block_number_available(web3, block_no))
 
             header = web3.eth.get_block(block_no)
             self.info(f"block_number: {header['number']}, gas_used: {header['gasUsed']}")
@@ -63,6 +79,7 @@ class ElBalanceTransferTest(testenv.StrataTester):
         self.info(f"total gas used: {total_gas_used}")
 
         assert total_gas_used <= EPOCH_GAS_LIMIT, "epoch gas should be limited"
+        assert total_gas_used < GAS_PER_TX * TX_COUNT, "all txns should NOT be processed"
 
 
 def make_gas_burner_transaction(
