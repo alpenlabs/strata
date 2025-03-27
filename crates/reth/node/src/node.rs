523caa60f54ae73fee402604ce266c07f41f6cb6
Sapin Bajracharya <sapin@alpenlabs.io>
2025-03-27 14:57:00 +0800

    rename: strata-reth-* to alpen-reth-*

--- crates/reth/node/src/node.rs
+++ crates/reth/node/src/node.rs
@@ -19,7 +19,7 @@ use reth_provider::{
 };
 use reth_transaction_pool::{PoolTransaction, TransactionPool};
 use revm_primitives::alloy_primitives;
-use strata_reth_rpc::{SequencerClient, StrataEthApi};
+use alpen_reth_rpc::{SequencerClient, StrataEthApi};
 
 use crate::{
     args::StrataNodeArgs,
