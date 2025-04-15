dcbc30834f93463289c9463dc6727e02df25e215
krsnapaudel <dilli@alpenlabs.io>
2025-04-15 10:40:16 +0545

    docstrings

--- bin/strata-cli/src/errors.rs
+++ bin/strata-cli/src/errors.rs
@@ -1,52 +1,43 @@
 use thiserror::Error;
 
+/// Strata CLI error types
 #[derive(Debug, Error)]
 pub enum CliError {
+    /// Errors related to user input
     #[error("Invalid input: {0}")]
     UserInput(#[from] UserInputError),
 
+    /// Internal errors while handling user requests
     #[error("Internal error: {0}")]
     Internal(#[source] anyhow::Error),
 }
 
+/// Errors related to user input
+///
+/// These are errors that can be avoided if the user provides expected input.
 #[derive(Debug, thiserror::Error)]
 pub enum UserInputError {
+    /// Unsupported language for printing the mnemonic
     #[error("unsupported language. Use --help to see available options.")]
     UnsupportedLanguage,
 
+    /// Unsupported network for the wallet, must be `signet` or `strata`
     #[error("unsupported network. Use --help to see available options.")]
     UnsupportedNetwork,
 
+    /// Wrong network for wallet address, e.g. `signet` address used in `strata` network
     #[error("wrong network for address")]
     WrongNetwork,
 
+    /// Invalid `signet` address
     #[error("invalid signet address. Must be a valid Bitcoin address.")]
     InvalidSignetAddress,
 
+    /// Invalid `strata` address
     #[error("invalid Strata address. Must be a valid EVM address.")]
     InvalidStrataAddress,
 
+    /// No address provided for `signet` or `strata` network
     #[error("either signet or Strata address must be provided")]
     MissingTargetAddress,
-}
-
-#[derive(Debug, Error)]
-pub enum WalletError {
-    #[error("wallet not initialized")]
-    NotInitialized,
-
-    #[error("failed to load wallet")]
-    LoadFailed,
-
-    #[error("failed to scan wallet")]
-    ScanFailed,
-
-    #[error("failed to sync wallet")]
-    SyncFailed,
-
-    #[error("failed to save wallet state")]
-    PersistFailed,
-
-    #[error("invalid wallet descriptor")]
-    InvalidWalletDescriptor,
 }
