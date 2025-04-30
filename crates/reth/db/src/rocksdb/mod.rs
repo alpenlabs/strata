684030f16b6b1bc926effa568857c859cf1d4e87
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2025-04-30 22:28:08 +0200

    Implement StateDiff traits for DB.

--- crates/reth/db/src/rocksdb/mod.rs
+++ crates/reth/db/src/rocksdb/mod.rs
@@ -5,7 +5,9 @@ mod schema;
 
 pub const ROCKSDB_NAME: &str = "express-reth";
 
-pub const STORE_COLUMN_FAMILIES: &[ColumnFamilyName] =
-    &[schema::BlockWitnessSchema::COLUMN_FAMILY_NAME];
+pub const STORE_COLUMN_FAMILIES: &[ColumnFamilyName] = &[
+    schema::BlockWitnessSchema::COLUMN_FAMILY_NAME,
+    schema::BlockStateDiffSchema::COLUMN_FAMILY_NAME,
+];
 
 pub use db::WitnessDB;
