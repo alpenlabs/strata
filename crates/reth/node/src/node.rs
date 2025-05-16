f3d552b141a75531a98d1c9f0bcaf898bcef6ae1
abishek <abishek@alpenlabs.io>
2025-05-16 14:01:31 +0545

    reth compiles

--- crates/reth/node/src/node.rs
+++ crates/reth/node/src/node.rs
@@ -12,12 +12,13 @@ use reth_node_builder::{
 };
 use reth_node_ethereum::{
     node::{
-        EthereumConsensusBuilder, EthereumEngineValidatorBuilder, EthereumNetworkBuilder,
+        EthereumAddOns, EthereumConsensusBuilder, EthereumEngineValidatorBuilder,
+        EthereumExecutorBuilder, EthereumNetworkBuilder, EthereumPayloadBuilder,
         EthereumPoolBuilder,
     },
-    BasicBlockExecutorProvider, EthereumEthApiBuilder,
+    BasicBlockExecutorProvider, EthEngineTypes, EthereumEthApiBuilder,
 };
-use reth_primitives::BlockBody;
+use reth_primitives::{BlockBody, EthPrimitives};
 use reth_provider::{
     providers::{ChainStorage, NodeTypesForProvider},
     BlockBodyReader, BlockBodyWriter, ChainSpecProvider, ChainStorageReader, ChainStorageWriter,
@@ -29,10 +30,8 @@ use revm_primitives::alloy_primitives;
 use strata_reth_rpc::{eth::StrataEthApiBuilder, SequencerClient, StrataEthApi};
 
 use crate::{
-    args::StrataNodeArgs,
-    engine::{StrataEngineTypes, StrataEngineValidator, StrataEngineValidatorBuilder},
-    evm::StrataEvmConfig,
-    payload_builder::StrataPayloadServiceBuilder,
+    args::StrataNodeArgs, evm::StrataEvmConfig, payload_builder::StrataPayloadServiceBuilder,
+    StrataPayloadTypes,
 };
 
 /// Strata primitive types.
@@ -108,3 +107,121 @@ pub struct StrataEthereumNode {
     // Strata node args.
     pub args: StrataNodeArgs,
 }
+
+impl StrataEthereumNode {
+    /// Creates a new instance of the StrataEthereum node type.
+    pub fn new(args: StrataNodeArgs) -> Self {
+        Self { args }
+    }
+
+    /// Returns a [`ComponentsBuilder`] configured for a regular Ethereum node.
+    pub fn components<Node>() -> ComponentsBuilder<
+        Node,
+        EthereumPoolBuilder,
+        BasicPayloadServiceBuilder<EthereumPayloadBuilder>,
+        EthereumNetworkBuilder,
+        EthereumExecutorBuilder,
+        EthereumConsensusBuilder,
+    >
+    where
+        Node: FullNodeTypes<
+            Types: NodeTypes<
+                Payload = EthEngineTypes,
+                ChainSpec = ChainSpec,
+                Primitives = StrataPrimitives,
+            >,
+        >,
+    {
+        ComponentsBuilder::default()
+            .node_types::<Node>()
+            .pool(EthereumPoolBuilder::default())
+            .payload(BasicPayloadServiceBuilder::default())
+            .network(EthereumNetworkBuilder::default())
+            .executor(EthereumExecutorBuilder::default())
+            .consensus(EthereumConsensusBuilder::default())
+    }
+}
+
+impl NodeTypes for StrataEthereumNode {
+    type Primitives = StrataPrimitives;
+    type ChainSpec = ChainSpec;
+    type StateCommitment = reth_trie_db::MerklePatriciaTrie;
+    type Storage = StrataStorage;
+    type Payload = EthEngineTypes;
+}
+
+impl<N> Node<N> for StrataEthereumNode
+where
+    N: FullNodeTypes<
+        Types: NodeTypes<
+            Payload = EthEngineTypes,
+            ChainSpec = ChainSpec,
+            Primitives = StrataPrimitives,
+            Storage = StrataStorage,
+        >,
+    >,
+{
+    type ComponentsBuilder = ComponentsBuilder<
+        N,
+        EthereumPoolBuilder,
+        BasicPayloadServiceBuilder<EthereumPayloadBuilder>,
+        EthereumNetworkBuilder,
+        EthereumExecutorBuilder,
+        EthereumConsensusBuilder,
+    >;
+
+    type AddOns = EthereumAddOns<
+        NodeAdapter<N, <Self::ComponentsBuilder as NodeComponentsBuilder<N>>::Components>,
+    >;
+
+    fn components_builder(&self) -> Self::ComponentsBuilder {
+        Self::components()
+    }
+
+    fn add_ons(&self) -> Self::AddOns {
+        Self::AddOns::default()
+    }
+}
+
+// #[derive(Debug, Default, Clone)]
+// pub struct StrataAddOns<N: FullNodeComponents> {
+//     pub rpc_add_ons: RpcAddOns<N, StrataEthApiBuilder, StrataEngineValidatorBuilder>,
+// }
+
+// #[derive(Debug, Default, Clone)]
+// #[non_exhaustive]
+// pub struct StrataAddOnsBuilder {
+//     /// Sequencer client, configured to forward submitted transactions to sequencer of given OP
+//     /// network.
+//     sequencer_client: Option<SequencerClient>,
+// }
+
+// impl StrataAddOnsBuilder {
+//     /// With a [`SequencerClient`].
+//     pub fn with_sequencer(mut self, sequencer_client: Option<String>) -> Self {
+//         self.sequencer_client = sequencer_client.map(SequencerClient::new);
+//         self
+//     }
+// }
+
+// impl StrataAddOnsBuilder {
+//     /// Builds an instance of [`StrataAddOns`].
+//     pub fn build<N>(self) -> StrataAddOns<N>
+//     where
+//         N: FullNodeComponents<Types: NodeTypes<Primitives = StrataPrimitives>>,
+//     {
+//         let Self { sequencer_client } = self;
+
+//         StrataAddOns {
+//             rpc_add_ons: RpcAddOns::new(
+//                 move |ctx| {
+//                     StrataEthApi::<N>::builder()
+//                         .with_sequencer(sequencer_client)
+//                         .build(ctx)
+//                 },
+//                 Default::default(),
+//                 Default::default(),
+//             ),
+//         }
+//     }
+// }
