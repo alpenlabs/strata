209ac3dbdddc71fa7678246d84ad24a86cb6ebdd
Sapin Bajracharya <sapin@alpenlabs.io>
2025-05-21 17:11:08 +0545

    save L1Block from reader task

--- crates/btcio/src/reader/reader_task.rs
+++ crates/btcio/src/reader/reader_task.rs
@@ -1,15 +1,16 @@
 use std::{ops::Range, sync::Arc, time::Duration};
 
-use bitcoin::Block;
+use bitcoin::{Block, BlockHash};
 use bitcoind_async_client::traits::Reader;
 use futures::{
     stream::{unfold, FuturesUnordered},
     Stream, StreamExt,
 };
-use strata_primitives::params::Params;
+use strata_config::btcio::ReaderConfig;
+use strata_primitives::{l1::L1Block, params::Params};
 use strata_state::l1::L1BlockId;
 use strata_storage::{L1BlockManager, NodeStorage};
-use tokio::{select, sync::mpsc};
+use tokio::{pin, select, sync::mpsc, time::sleep, try_join};
 use tracing::{debug, error, warn};
 
 pub enum ReaderCommand {
@@ -21,24 +22,31 @@ pub async fn reader_task(
     client: Arc<impl Reader>,
     storage: Arc<NodeStorage>,
     params: Arc<Params>,
+    config: Arc<ReaderConfig>,
     block_tx: mpsc::Sender<L1BlockId>,
     mut command_rx: mpsc::Receiver<ReaderCommand>,
 ) -> anyhow::Result<()> {
+    // TODO: replace with zmq block listener
     let start_height = storage
         .l1()
         .get_canonical_chain_tip_async()
         .await?
         .map(|(height, _)| height + 1)
         .unwrap_or(params.rollup.genesis_l1_height);
-
-    let block_stream = get_block_stream(start_height, client.clone());
-    tokio::pin!(block_stream);
+    let block_stream = get_block_stream(
+        start_height,
+        config.client_poll_dur_ms as u64,
+        client.clone(),
+    );
+    pin!(block_stream);
 
     loop {
         select! {
             block = block_stream.next() => {
-                let block = block.expect("block must exist");
-                if let Err(err) = handle_block(block, storage.l1(), &block_tx).await {
+                let (height, block) = block.expect("block must exist");
+                let l1block = L1Block::new(height, block);
+
+                if let Err(err) = handle_block(l1block, storage.l1(), &block_tx).await {
                     warn!("failed to process block: {:?}", err);
                 }
                 continue;
@@ -52,34 +60,23 @@ pub async fn reader_task(
     }
 }
 
-// TODO: replace with zmq block listener
-fn get_block_stream(start_height: u64, client: Arc<impl Reader>) -> impl Stream<Item = Block> {
-    unfold((start_height, client), |(next_height, client)| async move {
+fn get_block_stream(
+    start_height: u64,
+    poll_interval_ms: u64,
+    client: Arc<impl Reader>,
+) -> impl Stream<Item = (u64, Block)> {
+    unfold((start_height, client), move |(height, client)| async move {
         loop {
-            match client.get_block_at(next_height).await {
-                Ok(block) => return Some((block, (next_height + 1, client.clone()))),
+            match client.get_block_at(height).await {
+                Ok(block) => return Some(((height, block), (height + 1, client))),
                 Err(err) => {
                     dbg!(&err);
                     // TODO: distinguish recoverable and non-recoverable error
-                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    sleep(Duration::from_millis(poll_interval_ms)).await;
                 }
             };
         }
     })
-}
-
-async fn save_block(block: Block, l1: &L1BlockManager) -> anyhow::Result<L1BlockId> {
-    unimplemented!()
-}
-
-async fn handle_block(
-    block: Block,
-    l1: &L1BlockManager,
-    tx: &mpsc::Sender<L1BlockId>,
-) -> anyhow::Result<()> {
-    let blockid = save_block(block, l1).await?;
-    let _ = tx.send(blockid).await;
-    Ok(())
 }
 
 async fn handle_command(
@@ -90,8 +87,12 @@ async fn handle_command(
 ) {
     match command {
         ReaderCommand::FetchBlockById(block_id) => {
-            let block = match client.get_block(&block_id.into()).await {
-                Ok(block) => block,
+            let blockhash = BlockHash::from(block_id);
+            let block = match try_join!(
+                client.get_block_height(&blockhash),
+                client.get_block(&blockhash),
+            ) {
+                Ok((height, block)) => L1Block::new(height, block),
                 Err(err) => {
                     warn!(%block_id, "failed to fetch block: {:?}", err);
                     return;
@@ -112,7 +113,7 @@ async fn handle_command(
 
             while let Some((height, res)) = work.next().await {
                 let block = match res {
-                    Ok(block) => block,
+                    Ok(block) => L1Block::new(height as u64, block),
                     Err(err) => {
                         warn!(%height, "failed to fetch block: {:?}", err);
                         return;
@@ -124,4 +125,15 @@ async fn handle_command(
             }
         }
     }
+}
+
+async fn handle_block(
+    block: L1Block,
+    l1: &L1BlockManager,
+    tx: &mpsc::Sender<L1BlockId>,
+) -> anyhow::Result<()> {
+    let blockid = block.block_id();
+    l1.put_block_async(block).await?;
+    let _ = tx.send(blockid).await;
+    Ok(())
 }
